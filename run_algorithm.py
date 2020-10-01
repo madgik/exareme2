674d3c71a0a88eab18779e698ea9951fa7798435
johnfouf <johnfouf@gmail.com>
2020-10-01 14:21:11 +0300

    Update run_algorithm.py

--- run_algorithm.py
+++ run_algorithm.py
@@ -46,17 +46,6 @@ async def dataflow(algorithm, parameters, attr, db_objects, localtable, globalta
     return await func[0](algorithm, parameters, attr, db_objects, localtable, globaltable,  viewlocaltable, localschema, globalresulttable, globalschema)
 
 
-###### this contains an example of how the dataflow looks like in this layer
-async def static_dataflow(algorithm, parameters, attr, db_objects, localtable, globaltable,  viewlocaltable, localschema, globalresulttable = None, globalschema = None):
-    await task._init(db_objects,localtable,localschema,globalresulttable,globalschema)
-    for iternum in range(100):
-        await task._local(iternum, globalresulttable, parameters, attr, db_objects,localtable, algorithm, viewlocaltable, localschema)
-        res = await task._global(iternum, globaltable, parameters, attr, db_objects, localtable, globalresulttable, algorithm, viewlocaltable, globalschema)
-        if res[0][0] > 1000000:
-            break
-    return res
-        
-
 
 #### run function:
 # get the algorithm name and accesses the corresponding python module
