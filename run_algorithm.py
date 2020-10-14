550310f072f1b9af18f92dc135d12b1cc5177ea5
openaire <openaire@dl126.madgik.di.uoa.gr>
2020-10-15 00:51:25 +0300

    flow of tasks is now defined by the algorithm developer without having any acess to the connection objects or other system internals, using python's generator features

--- run_algorithm.py
+++ run_algorithm.py
@@ -24,69 +24,6 @@ def get_uniquetableid():
 
 
 
-###########
-# this function gets the dataflow definition from the [algorithm].py file and replaces local and global calls with the corrresponding calls that 
-# run by the task executor and handle the database objects and the parallelism. This is not called in the current implementation but it gives just
-# a description of  the logic. Currently it is done with regular expression and this is the bad way.
-# Other options are using Python's parser module to edit the byte code but this is monkey patching and requires a lot of attention, or using classes and decorators
-# but in this case several issues occur (e.g., should an algorithm developed outside the system be able to have access to the system's objects like the connection objects of the DBs?)
-# Another perhaps cleaner option is to support a subset of Python or any other dataflow language which is enough for a developer to use the system defined tasks and produce any possible dataflow.
-# Currently there are 2 system defined tasks: 1) _local: runs a task in all the local nodes 2) _global merges local results and runs a task on the global server.
-# Other system defined tasks should be added (e.g., run a task to 1 or N local nodes) so that the algorithm developer is able to define any kind of data flow.
-# When in production this function probably will act as a parser of a user defined dataflow and an interpreter that interprets this dataflow to the system's internal flow of tasks.
-# In this way, we are able to separate the algorithm from the system's internals, so that it is simply an input to the system and agnostic to the techniques  the system uses to implement the dataflows.
-async def dataflow_parse_and_execute(dataflow,  task_executor):
-    dataflow_source_input = inspect.getsource(dataflow)
-    dataflow_func = [0]
-
-    ###### of course this MUST change ########################
-    edited_source = re.sub(
-        "self\.\_global\(iternum(?:\s)*\,(?:\s)*globaltable(?:\s)*\,(?:\s)*parameters(?:\s)*\,(?:\s)*attributes(?:\s)*\)",
-        "await task_executor._global(iternum)",
-        dataflow_source_input,
-    )
-    edited_source = re.sub(
-        "self\.\_local\(iternum(?:\s)*\,(?:\s)*viewlocaltable(?:\s)*\,(?:\s)*parameters(?:\s)*\,(?:\s)*attributes(?:\s)*,(?:\s)*globalresulttable\)",
-        "await task_executor._local(iternum)",
-        edited_source,
-    )
-    edited_source = edited_source.split("\n", 1)[-1]
-    edited_source = (
-        "async def dataflow(task_executor):\n"
-        + edited_source
-        + "\ndataflow_func[0] = dataflow"
-    )
-    #########################################################################################
-
-    abstract_syntax_tree = parser.suite(edited_source)
-    exec(parser.compilest(abstract_syntax_tree))
-    return await dataflow_func[0](task_executor)
-
-
-# this is an example dataflow for pearson, this function is not called in the current execution flow, 
-# to run pearson you should edit the call to dataflow function in run
-async def dataflow_pearson(task_executor):
-    iternum = 0
-    await task_executor._local(iternum)
-    return await task_executor._global(iternum)
-
-
-async def dataflow_countiter(task_executor):    #### this  is an example dataflow for countiter
-        res = 0
-        for iternum in range(200):
-            await task_executor._local(iternum)
-            res = await task_executor._global(iternum)
-            if res[0][0] > 1000000:
-                break
-        return res
-
-#### run function:
-# creates unique table names
-# get the algorithm name and accesses the corresponding python module
-# creates the local views
-# gets also db_objects: the connection objects to all the nodes of the federation
-# runs the dataflow
-# cleans up the servers and returns the results
 
 
 async def dataflow(task_executor, algorithm, params, table_id):
@@ -119,16 +56,16 @@ async def run(algorithm, params, db_objects):
 
     module = get_package(algorithm)
     algorithm_instance = module.Algorithm()
-    unique_id = get_uniquetableid()
-    transfer_runner = transfer.Transfer(db_objects, unique_id)
-    task_executor = task.Task(db_objects, unique_id, algorithm_instance, params,  transfer_runner)
+    table_id = get_uniquetableid()
+    transfer_runner = transfer.Transfer(db_objects, table_id)
+    task_executor = task.Task(db_objects, table_id, algorithm_instance, params,  transfer_runner)
 
     # create database views on local databases - each view processes the filters and the selected attributes on the requested table
     # the algorithm won't run directly on the local dataset but on the view
     await task_executor.createlocalviews()
 
     try:
-        result  = await dataflow(task_executor, algorithm_instance.algorithm, params, unique_id)
+        result  = await dataflow(task_executor, algorithm_instance.algorithm, params, table_id)
     except:
         #### clean unused tables
         await task_executor.clean_up()
