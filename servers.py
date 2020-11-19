f390bc9fe2577c18b9248d854c46da57970f7da2
ThanKarab <tkarabatsis@hotmail.com>
2020-11-19 18:36:41 +0200

    Fixed monetdb dockerised. 1) Changed python to 3.8 (3.9 doesn't work yet). 2) Added python-pip. 3) numpy, scipy , etc added as well.

--- servers.py
+++ servers.py
@@ -1,6 +1,6 @@
 # servers = ["postgres://88.197.53.126:5432/guru99", "postgres://88.197.53.136:5433/template1","postgres://88.197.53.126:5432/template1"]
 servers = [
-    "monetdb://127.0.0.1:50000/test1",
-    "monetdb://127.0.0.1:50000/test2",
-    "monetdb://127.0.0.1:50000/test3",
+    "monetdb://192.168.63.129:50000/db",
+    "monetdb://192.168.63.129:50001/db",
+    "monetdb://192.168.63.129:50002/db",
 ]
