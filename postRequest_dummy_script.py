0298ebc6ac7d244306ab1d3c838e9bc775acefdd
simos <simospm@di.uoa.gr>
2020-11-30 15:47:14 +0200

    cleanup, deleted some files

--- postRequest_dummy_script.py
+++ postRequest_dummy_script.py
@@ -6,9 +6,7 @@ json={
     "algorithmParams": {
         "algorithmFolder":"dummy",
         "algorithmFlowFile": "dummy_flow",
-        "algorithmUDFsFile":"dummy_udfs",
-        "input_par1": 1.234,
-        "input_par2":5.678
+        "algorithmUDFsFile":"dummy_udfs"
     },
     "dataParams": {
         "table": "data",
