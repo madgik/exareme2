6870066b323bb8c51509d7855dccc955b3981501
openaire <openaire@dl126.madgik.di.uoa.gr>
2020-12-07 19:07:54 +0200

    add some ideas on how python2sql could be compatible

---
+++ algorithms/dummy_python.py
@@ -0,0 +1,18 @@
+import python2monetsql
+
+class Algorithm:
+
+    def algorithm(self, data_table, merged_local_results, parameters, attributes, result_table):
+        for iternum in range(60):
+            yield python2sql.to_sql(local_step)
+            yield python2sql.to_sql(global_step)
+
+
+    def local_step(self):
+        ## execution of a local step
+        pass
+
+
+    def global_step(self):
+        ## execution of a global step
+        pass
---
+++ algorithms/python2monetsql.py
@@ -0,0 +1,3 @@
+def to_sql(func):
+    # code to transform python's input into sqlscript
+    return sqlscript
