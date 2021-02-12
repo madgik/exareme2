0104c0ecda4b59b89762d8648a7fe229bdc3f925
ThanKarab <tkarabatsis@hotmail.com>
2021-02-12 15:36:58 +0200

    Added keyword arguments on long method calls.

--- mipengine/controller/api/DTOs/AlgorithmSpecificationsDTOs.py
+++ mipengine/controller/api/DTOs/AlgorithmSpecificationsDTOs.py
@@ -37,37 +37,37 @@ class InputDataSpecificationDTO:
 
 def get_pathology_parameter():
     return InputDataSpecificationDTO(
-        "Pathology of the data.",
-        "The pathology that the algorithm will run on.",
-        ["text"],
-        True,
-        False,
-        None,
-        None,
+        label="Pathology of the data.",
+        desc="The pathology that the algorithm will run on.",
+        types=["text"],
+        notblank=True,
+        multiple=False,
+        stattypes=None,
+        enumslen=None,
     )
 
 
 def get_dataset_parameter():
     return InputDataSpecificationDTO(
-        "Set of data to use.",
-        "The set of data to run the algorithm on.",
-        ["text"],
-        True,
-        True,
-        None,
-        None,
+        label="Set of data to use.",
+        desc="The set of data to run the algorithm on.",
+        types=["text"],
+        notblank=True,
+        multiple=True,
+        stattypes=None,
+        enumslen=None,
     )
 
 
 def get_filter_parameter():
     return InputDataSpecificationDTO(
-        "Filter on the data.",
-        "Features used in my algorithm.",
-        ["jsonObject"],
-        False,
-        False,
-        None,
-        None,
+        label="Filter on the data.",
+        desc="Features used in my algorithm.",
+        types=["jsonObject"],
+        notblank=False,
+        multiple=False,
+        stattypes=None,
+        enumslen=None,
     )
 
 
