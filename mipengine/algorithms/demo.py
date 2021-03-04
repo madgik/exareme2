679224f2a3312b00c7e655a4e09925aa98b79bf9
Jason Sakellariou <js.sakel@gmail.com>
2021-03-04 18:14:31 +0200

    Add select statement for calling table returning UDFs

--- mipengine/algorithms/demo.py
+++ mipengine/algorithms/demo.py
@@ -1,6 +1,7 @@
 from mipengine.algorithms.iotypes import udf
 from mipengine.algorithms.iotypes import TableT
 from mipengine.algorithms.iotypes import TensorT
+from mipengine.algorithms.iotypes import ScalarT
 
 
 @udf
@@ -12,4 +13,10 @@ def func(x: TableT, y: TableT) -> TableT:
 @udf
 def tensor3(t1: TensorT, t2: TensorT, t3: TensorT) -> TensorT:
     result = t1 @ t2 @ t3
+    return result
+
+
+@udf
+def tensor1(t: TensorT) -> ScalarT:
+    result = 1
     return result
--- mipengine/node/udfgen/selstmt.py
+++ mipengine/node/udfgen/selstmt.py
@@ -20,22 +20,9 @@ SEP = ", "
 SEPLN = ",\n"
 LN = "\n"
 SELECT = "SELECT"
+STAR = " * "
 FROM = "FROM"
 WHERE = "WHERE"
-
-
-def all_equal(iterable, func=None):
-    """Returns True if all elements in iterable are equal, False otherwise. If
-    func is passed a new iterable is used by mapping func to iterable."""
-    itr = map(func, iterable) if func else iterable
-    g = groupby(itr)
-    return next(g, True) and not next(g, False)
-
-
-def pretify(lst_expr):
-    if len(lst_expr) > 80:
-        return LN + indent(lst_expr.replace(SEP, SEPLN), PRFX) + LN
-    return lst_expr
 
 
 def generate_udf_select_stmt(
@@ -44,8 +31,28 @@ def generate_udf_select_stmt(
     positional_args: list[TableInfo],
     keyword_args: dict[str, TableInfo],  # XXX not used for now
 ) -> str:
+    """
+    Generates select statement for calling MonetDB Python UDFs.
+
+    Parameters
+    ----------
+        func_name: str
+            Name of function from which to generate UDF.
+        udf_name: str
+            Name to use in UDF definition.
+        positional_args: list[dict]
+            Positional parameter info objects.
+        keyword_args: dict[str, dict]
+            Keyword parameter info objects.
+
+    Returns
+    -------
+        str
+            Multiline string with select statemet calling a MonetDB Python UDF.
+    """
     gen = UDFGEN_REGISTRY.get(func_name, None) or get_generator(func_name)
     parameters = gen.signature.parameters
+    return_hint = gen.signature.return_annotation
     type_hints = {name: parameter.annotation for name, parameter in parameters.items()}
 
     if TableT in type_hints.values():
@@ -68,10 +75,10 @@ def generate_udf_select_stmt(
         for table, columns in zip(table_names, table_schemas)
         for column in columns
     ]
-    udf_call_args = pretify(SEP.join(udf_arguments))
+    udf_call_args = prettify(SEP.join(udf_arguments))
     udf_call = f"{udf_name}({udf_call_args})"
 
-    from_subexpr = pretify(SEP.join(table_names))
+    from_subexpr = prettify(SEP.join(table_names))
 
     if TableT in type_hints.values():
         head_table, *tail_tables = table_names
@@ -92,14 +99,53 @@ def generate_udf_select_stmt(
     else:
         raise NotImplementedError
 
-    select_stmt = [SELECT]
-    select_stmt.append(indent(udf_call, PRFX))
-    select_stmt.append(FROM)
-    select_stmt.append(indent(from_subexpr, PRFX))
-    select_stmt.append(WHERE)
-    select_stmt.append(indent(where_subexpr, PRFX))
+    if return_hint not in (TableT, TensorT):
+        select_lines = [SELECT]
+        select_lines.append(indent(udf_name + parens(udf_call_args), PRFX))
+        # select_lines.append(indent(udf_call, PRFX))
+        select_lines.append(FROM)
+        select_lines.append(indent(from_subexpr, PRFX))
+        if where_subexpr:
+            select_lines.append(WHERE)
+            select_lines.append(indent(where_subexpr, PRFX))
+        select_stmt = LN.join(select_lines)
 
-    return LN.join(select_stmt)
+    else:
+        select_lines = [SELECT + STAR]
+        select_lines.append(FROM)
+        subquery_lines = [SELECT]
+        subquery_lines.append(indent(udf_call_args, PRFX))
+        subquery_lines.append(FROM)
+        subquery_lines.append(indent(from_subexpr, PRFX))
+        if where_subexpr:
+            subquery_lines.append(WHERE)
+            subquery_lines.append(indent(where_subexpr, PRFX))
+        subquery = LN.join(subquery_lines)
+        select_lines.append(indent(udf_name + parens(parens(subquery)), PRFX))
+        select_stmt = LN.join(select_lines)
+
+    return select_stmt
+
+
+def all_equal(iterable, func=None):
+    """Returns True if all elements in iterable are equal, False otherwise. If
+    func is passed a new iterable is used by mapping func to iterable."""
+    itr = map(func, iterable) if func else iterable
+    g = groupby(itr)
+    return next(g, True) and not next(g, False)
+
+
+def prettify(lst_expr):
+    if len(lst_expr) > 80:
+        return LN + indent(lst_expr.replace(SEP, SEPLN), PRFX) + LN
+    return lst_expr
+
+
+def parens(expr):
+    if LN not in expr and len(expr) <= 78:
+        return "(" + expr + ")"
+    else:
+        return "(\n" + expr + "\n)"
 
 
 class ColumnInfo:
@@ -125,8 +171,11 @@ if __name__ == "__main__":
     sel = generate_udf_select_stmt("demo.func", "example", [t1, t2], {})
     print(sel)
     print("-" * 80)
-    t1 = TableInfo(int, "tab1", ["dim0", "dim1", "dim2", "val"], 10)
-    t2 = TableInfo(int, "tab2", ["dim0", "dim1", "dim2", "val"], 10)
-    t3 = TableInfo(int, "tab3", ["dim0", "dim1", "dim2", "val"], 10)
+    t1 = TableInfo(int, "T1", ["dim0", "dim1", "dim2", "val"], 8)
+    t2 = TableInfo(int, "T1", ["dim0", "dim1", "dim2", "val"], 8)
+    t3 = TableInfo(int, "T1", ["dim0", "dim1", "dim2", "val"], 8)
     sel = generate_udf_select_stmt("demo.tensor3", "example", [t1, t2, t3], {})
+    print(sel)
+    print("-" * 80)
+    sel = generate_udf_select_stmt("demo.tensor1", "func", [t1], {})
     print(sel)
