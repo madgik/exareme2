ce210e0256ae4c96d92daec3408a75438c20ef8f
ThanKarab <tkarabatsis@hotmail.com>
2021-04-13 14:56:02 +0300

    Added celery time limits in the config file.

--- mipengine/node/celery_config.py
+++ mipengine/node/celery_config.py
@@ -1,3 +1,5 @@
+from mipengine import config
+
 worker_concurrency = 1
-task_soft_time_limit = 30
-task__time_limit = 60
+task_soft_time_limit = config.node.tasks_soft_time_limit
+task_time_limit = config.node.tasks_time_limit
