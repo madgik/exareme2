441ee754ee4d5076d34da3ebc146fb577e600e70
simos <simospm@di.uoa.gr>
2021-05-14 01:13:24 +0300

    implemented as a class

--- mipengine/node_registry/node_registry.py
+++ mipengine/node_registry/node_registry.py
@@ -9,112 +9,113 @@ from mipengine.common.node_registry_DTOs import (
     NodeRole,
 )
 
-c = consul.Consul()
-consul_agent = c.agent
-consul_service = consul_agent.service
-kv_store = c.kv
 
+class NodeRegistryClient:
+    def __init__(
+        self,
+        consul_server_ip: IPv4Address = IPv4Address("127.0.0.1"),
+        consul_server_port: int = 8500,
+    ):
+        c = consul.Consul(host=consul_server_ip, port=consul_server_port)
+        self._consul_agent = c.agent
+        self._consul_service = c.agent.service
+        self._consul_kv_store = c.kv
 
-def register_node(node_record: NodeRecord):
-    # register the node as a service
-    consul_service.register(
-        name=node_record.node_id,
-        service_id=node_record.node_id,
-        address=str(node_record.task_queue_ip),
-        port=node_record.task_queue_port,
-        tags=(node_record.node_role.name,),
-    )
-    # register the db as a service
-    consul_service.register(
-        name=node_record.db_id,
-        service_id=node_record.db_id,
-        address=str(node_record.db_ip),
-        port=node_record.db_port,
-        tags=("db",),
-    )
+    def register_node(self, node_record: NodeRecord):
+        # register the node as a service
+        self._consul_service.register(
+            name=node_record.node_id,
+            service_id=node_record.node_id,
+            address=str(node_record.task_queue_ip),
+            port=node_record.task_queue_port,
+            tags=(node_record.node_role.name,),
+        )
+        # register the db as a service
+        self._consul_service.register(
+            name=node_record.db_id,
+            service_id=node_record.db_id,
+            address=str(node_record.db_ip),
+            port=node_record.db_port,
+            tags=("db",),
+        )
 
-    # The node's db is linked to the node by storing the db_id as a value in the key
-    # value store of consul
-    # The node's pathologies are also stored in the consul key/value store
-    node_configuration = NodeConfiguration(
-        db_id=node_record.db_id, pathologies=node_record.pathologies
-    )
-    kv_store.put(node_record.node_id, node_configuration.json())
+        # The node's db is linked to the node by storing the db_id as a value in the key
+        # value store of consul
+        # The node's pathologies are also stored in the consul key/value store
+        node_configuration = self.NodeConfiguration(
+            db_id=node_record.db_id, pathologies=node_record.pathologies
+        )
+        self._consul_kv_store.put(node_record.node_id, node_configuration.json())
 
+    def deregister_node(self, node_id: str):
+        _, data = self._consul_kv_store.get(node_id)
 
-def deregister_node(node_id: str):
-    _, data = kv_store.get(node_id)
-
-    if not data:
-        raise NodeIDNotInKVStore(node_id)
-
-    node_conf = NodeConfiguration.parse_raw(data["Value"])
-
-    # deregister db service
-    consul_service.deregister(node_conf.db_id)
-
-    # deregister node service
-    consul_service.deregister(service_id=node_id)
-
-    # delete node configuration from kv store
-    kv_store.delete(node_id)
-
-
-def get_all_nodes() -> Dict[str, "NodeInfo"]:
-    all_services = consul_agent.services()
-    node_roles_str = [node_role.name for node_role in list(NodeRole)]
-    all_nodes = {
-        service_id: NodeInfo(ip=service_info["Address"], port=service_info["Port"])
-        for (service_id, service_info) in all_services.items()
-        if any(x in service_info["Tags"] for x in node_roles_str)
-    }
-
-    # pathologies are read from the key value store
-    for node_id in all_nodes.keys():
-        _, data = kv_store.get(node_id)
         if not data:
             raise NodeIDNotInKVStore(node_id)
-        node_conf = NodeConfiguration.parse_raw(data["Value"])
-        all_nodes[node_id].pathologies = node_conf.pathologies
 
-    return all_nodes
+        node_conf = self.NodeConfiguration.parse_raw(data["Value"])
 
+        # deregister db service
+        self._consul_service.deregister(node_conf.db_id)
 
-def get_db_info(node_id: str) -> "DBInfo":
-    _, data = kv_store.get(node_id, index=None)
+        # deregister node service
+        self._consul_service.deregister(service_id=node_id)
 
-    if not data:
-        raise NodeIDNotInKVStore(node_id)
+        # delete node configuration from kv store
+        self._consul_kv_store.delete(node_id)
 
-    node_conf = NodeConfiguration.parse_raw(data["Value"])
-    db_id = node_conf.db_id
+    def get_all_nodes(self) -> Dict[str, "NodeInfo"]:
+        all_services = self._consul_agent.services()
+        node_roles_str = [node_role.name for node_role in list(NodeRole)]
+        all_nodes = {
+            service_id: self.NodeInfo(
+                ip=service_info["Address"], port=service_info["Port"]
+            )
+            for (service_id, service_info) in all_services.items()
+            if any(x in service_info["Tags"] for x in node_roles_str)
+        }
 
-    all_services = consul_agent.services()
-    db_service = all_services[db_id]
+        # pathologies are read from the key value store
+        for node_id in all_nodes.keys():
+            _, data = self._consul_kv_store.get(node_id)
+            if not data:
+                raise self.NodeIDNotInKVStore(node_id)
+            node_conf = self.NodeConfiguration.parse_raw(data["Value"])
+            all_nodes[node_id].pathologies = node_conf.pathologies
 
-    db_info = DBInfo(
-        id=db_service["ID"], ip=db_service["Address"], port=db_service["Port"]
-    )
-    return db_info
+        return all_nodes
 
+    def get_db_info(self, node_id: str) -> "DBInfo":
+        _, data = self._consul_kv_store.get(node_id, index=None)
 
-class NodeIDNotInKVStore(Exception):
-    def __init__(self, node_id: str):
-        self.message = f"There is no node_id:{node_id} key in the key/value store"
+        if not data:
+            raise self.NodeIDNotInKVStore(node_id)
 
+        node_conf = self.NodeConfiguration.parse_raw(data["Value"])
+        db_id = node_conf.db_id
 
-class NodeInfo(BaseModel):
-    ip: IPv4Address
-    port: int
-    pathologies: Optional[List[Pathology]]
+        all_services = self._consul_agent.services()
+        db_service = all_services[db_id]
 
+        db_info = self.DBInfo(
+            id=db_service["ID"], ip=db_service["Address"], port=db_service["Port"]
+        )
+        return db_info
 
-class NodeConfiguration(BaseModel):
-    db_id: str
-    pathologies: Optional[List[Pathology]]
+    class NodeIDNotInKVStore(Exception):
+        def __init__(self, node_id: str):
+            self.message = f"There is no node_id:{node_id} key in the key/value store"
 
+    class NodeInfo(BaseModel):
+        ip: IPv4Address
+        port: int
+        pathologies: Optional[List[Pathology]]
 
-class DBInfo(BaseModel):
-    id: str
-    ip: IPv4Address
-    port: int
+    class NodeConfiguration(BaseModel):
+        db_id: str
+        pathologies: Optional[List[Pathology]]
+
+    class DBInfo(BaseModel):
+        id: str
+        ip: IPv4Address
+        port: int
--- test_script_node_registry.py
+++ test_script_node_registry.py
@@ -1,4 +1,4 @@
-import mipengine.service_registry.service_registry as sr
+from mipengine.node_registry.node_registry import NodeRegistryClient
 from mipengine.common.node_registry_DTOs import NodeRecord, Pathology, NodeRole
 from ipaddress import IPv4Address
 
@@ -26,37 +26,42 @@ a_node_record_2 = NodeRecord(
     pathologies=[a_pathology2],
 )
 
-# register 2 nodes
-sr.register_node(a_node_record_1)
-sr.register_node(a_node_record_2)
+nrclient = NodeRegistryClient(consul_server_ip="127.0.0.1", consul_server_port=8200)
+# nrclient = NodeRegistryClient()
 
-# get all registered nodes 
-all_nodes=sr.get_all_nodes()
-print("get_all_nodes() returns:")
-{print(f"{node_id=}\n{node_info=}") for (node_id,node_info) in all_nodes.items()}
+# register 2 nodes
+print("\nREGISTERING node1, node2...")
+nrclient.register_node(a_node_record_1)
+nrclient.register_node(a_node_record_2)
+
+# get all registered nodes
+all_nodes = nrclient.get_all_nodes()
+print("\nREGISTERED NODES:")
+{print(f"{node_id=}\n{node_info=}") for (node_id, node_info) in all_nodes.items()}
 
 # when a node is registered, its database gets also registered
 # get info about the db associated with this node
 try:
-    db_info=sr.get_db_info("node1")
-    print(f"\n{db_info=}")
-except sr.NodeIDNotInKVStore as exc:
+    db_info = nrclient.get_db_info("node1")
+    print(f"\nDB_INFO for node1: \n{db_info}")
+except nrclient.NodeIDNotInKVStore as exc:
     print(f"{exc.message=}")
 
 # deregister a node
 try:
-    sr.deregister_node("node1")
-except sr.NodeIDNotInKVStore as exc:
+    nrclient.deregister_node("node1")
+    print("\nDEREGISTERED node1")
+except nrclient.NodeIDNotInKVStore as exc:
     print(f"{exc.message=}")
 
 # deregistering a node deregisters the associated db as well
 try:
-    db_info=sr.get_db_info("node1")
-    print(f"\n{db_info=}")
-except sr.NodeIDNotInKVStore as exc:
+    db_info = nrclient.get_db_info("node1")
+except nrclient.NodeIDNotInKVStore as exc:
+    print("FAILED TO GET DB INFO for node1")
     print(f"{exc.message=}")
-    
 
-all_nodes=sr.get_all_nodes()
-print("get_all_nodes() returns:")
-{print(f"{node_id=}\n{node_info=}") for (node_id,node_info) in all_nodes.items()}
+
+all_nodes = nrclient.get_all_nodes()
+print(f"\nREGISTERED NODES:")
+{print(f"{node_id=}\n{node_info=}") for (node_id, node_info) in all_nodes.items()}
