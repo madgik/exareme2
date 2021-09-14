783cfa53d0c57b296d2bb4dd9ba541712c5e9613
Kfilippopolitis <kostasfilippop@gmail.com>
2021-09-14 19:24:14 +0300

    Poc Pydantic

--- mipengine/node_tasks_DTOs.py
+++ mipengine/node_tasks_DTOs.py
@@ -1,87 +1,73 @@
-from dataclasses import dataclass
 from typing import Any
 from typing import Dict
 from typing import List
 from typing import Union
+from warnings import warn
 
-from dataclasses_json import dataclass_json
+from pydantic import (
+    BaseModel,
+    validator,
+)
 
 
-@dataclass_json
-@dataclass
-class ColumnInfo:
+def validate_allowed_types(type: str, allowed_types: List[str]):
+    if type.lower() not in allowed_types:
+        raise TypeError(f"The allowed types are the following : {allowed_types}")
+    return type.lower()
+
+
+def validate_name(name):
+    if not name.isidentifier():
+        raise ValueError(f"Expected valid identifier, got {name}")
+    if not name.islower():
+        warn(f"Names must be lowercase, got {name}")
+        return name.lower()
+    return name
+
+
+class ColumnInfo(BaseModel):
     name: str
     data_type: str
 
-    def __post_init__(self):
-        allowed_types = {"int", "text", "real"}
-        self.data_type = str.lower(self.data_type)
-        if self.data_type not in allowed_types:
-            raise TypeError(
-                f"Column can have one of the following types: {allowed_types}"
-            )
-
-        if not self.name.isidentifier():
-            raise ValueError(
-                f"Name : {self.name} has inappropriate characters for a sql query."
-            )
+    _validate_allowed_types = validator(
+        "data_type", {"int", "text", "real"}, allow_reuse=True
+    )(validate_allowed_types)
+    _validate_name = validator("name", allow_reuse=True)(validate_name)
 
 
-@dataclass_json
-@dataclass
-class TableSchema:
+class TableSchema(BaseModel):
     columns: List[ColumnInfo]
 
 
-@dataclass_json
-@dataclass
-class TableInfo:
+class TableInfo(BaseModel):
     name: str
     schema: TableSchema
 
-    def __post_init__(self):
-        if not self.name.isidentifier():
-            raise ValueError(
-                f"Name : {self.name} has inappropriate characters for a sql query."
-            )
+    _validate_name = validator("name", allow_reuse=True)(validate_name)
 
 
-@dataclass_json
-@dataclass
-class TableView:
+class TableView(BaseModel):
     datasets: List[str]
     columns: List[str]
     filter: Dict
 
-    def __post_init__(self):
-        for dataset in self.datasets:
-            if not self.dataset.isidentifier():
-                raise ValueError(
-                    f"Dataset : {dataset} has inappropriate characters for a sql query."
-                )
-        for column in self.columns:
-            if not self.column.isidentifier():
-                raise ValueError(
-                    f"Column : {column} has inappropriate characters for a sql query."
-                )
+    _validate_names = validator(
+        "datasets",
+        "columns",
+        each_item=True,
+        allow_reuse=True,
+    )(validate_name)
 
 
-@dataclass_json
-@dataclass
-class TableData:
+class TableData(BaseModel):
     schema: TableSchema
     data: List[List[Union[str, int, float, bool]]]
 
 
-@dataclass_json
-@dataclass
-class UDFArgument:
+class UDFArgument(BaseModel):
     type: str
     value: Any
 
-    def __post_init__(self):
-        allowed_types = {"table", "literal"}
-        if self.type not in allowed_types:
-            raise TypeError(
-                f"UDFArgument type  can have one of the following types: {allowed_types}"
-            )
+    _validate_allowed_types = validator("type", {"table", "literal"}, allow_reuse=True)(
+        validate_allowed_types
+    )
