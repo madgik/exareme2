8e10eab793c81625b92e43aff45f5daaefc20082
ThanKarab <tkarabatsis@hotmail.com>
2021-10-07 09:51:12 +0300

    Added privacy constraint.

--- mipengine/node/monetdb_interface/views.py
+++ mipengine/node/monetdb_interface/views.py
@@ -4,6 +4,8 @@ from mipengine.filters import build_filter_clause
 from mipengine.node.monetdb_interface.common_actions import get_table_names
 from mipengine.node.monetdb_interface.monet_db_connection import MonetDB
 
+PRIVACY_MAGIC_NUMBER = 10
+
 
 def get_view_names(context_id: str) -> List[str]:
     return get_table_names("view", context_id)
@@ -28,3 +30,15 @@ def create_view(
         {filter_clause}
         """
     )
+
+    view_rows_query_result = MonetDB().execute_and_fetchall(
+        f"""
+        SELECT COUNT(*)
+        FROM {view_name}
+        """
+    )
+    view_rows_result_row = view_rows_query_result[0]
+    view_rows_count = view_rows_result_row[0]
+
+    if view_rows_count < PRIVACY_MAGIC_NUMBER:
+        raise ValueError("Privacy Error!!!")
