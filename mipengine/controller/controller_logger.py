5ce55fe688cbe0fc4964d9abb3467f0b0e089323
ThanKarab <tkarabatsis@hotmail.com>
2021-11-26 21:54:06 +0200

    Removed controller unit test, and log_method_call. Not useful currently and cannot be tested.

--- mipengine/controller/controller_logger.py
+++ mipengine/controller/controller_logger.py
@@ -9,20 +9,3 @@ def getAppLogger():
 
 def getServerLogger():
     return logging.getLogger("quart.server")
-
-
-def log_method_call(func):
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        logger = getAppLogger()
-        starting_timestamp = time.time()
-        logger.info(f"*********** {func.__name__} method started ***********")
-        output = func(*args, **kwargs)
-        finish_timestamp = time.time()
-        tsm_diff = finish_timestamp - starting_timestamp
-        logger.info(
-            f"*********** {func.__name__} method succeeded in {tsm_diff} ***********"
-        )
-        return output
-
-    return wrapper
