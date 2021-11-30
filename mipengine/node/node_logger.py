24a798208b947423b25300e1683421857f2cfafb
molly <katsouli.mo@gmail.com>
2021-11-30 18:29:06 +0200

    Changes in node logger to show context id in node message

--- mipengine/node/node_logger.py
+++ mipengine/node/node_logger.py
@@ -1,17 +1,57 @@
+import inspect
+import logging
 import time
 from functools import wraps
 
-from celery.utils.log import get_task_logger
+from mipengine.node import config as node_config
 
 
-def getLogger(name):
-    return get_task_logger(name)
+def init_logger(ctx_id=None):
+    logger = logging.getLogger("node")
+    if ctx_id == None:
+        formatter = logging.Formatter(f"%(message)s")
+    else:
+        formatter = logging.Formatter(
+            f"%(asctime)s - %(levelname)s - NODE - ctx_id: {ctx_id} - {node_config.role} - {node_config.identifier} - %(name)s - %(message)s"
+        )
+
+    # StreamHandler
+    sh = logging.StreamHandler()
+    sh.setFormatter(formatter)
+    for hdlr in logger.handlers[:]:  # remove all old handlers
+        logger.removeHandler(hdlr)
+    logger.addHandler(sh)
+    logger.setLevel(node_config.log_level)
+
+    return logger
+
+
+def get_logger():
+    return logging.getLogger("node")
+
+
+# we accept that context_id is always the first argument in a shared task function call
+def log_add_ctx_id(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        arglist = inspect.getfullargspec(func)
+        if kwargs.get("context_id"):
+            ctx_id = kwargs.get("context_id")
+        elif "context_id" in arglist.args:
+            ctx_id = args[0]
+        else:
+            ctx_id = None
+
+        init_logger(ctx_id)
+        func(*args, **kwargs)
+
+    return wrapper
 
 
 def log_method_call(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
-        logger = getLogger(func.__name__)
+        logger = get_logger()
         starting_timestamp = time.time()
         logger.info(f"*********** {func.__name__} method started ***********")
         output = func(*args, **kwargs)
