f52a59e84d329025094f85435f4b5a0a9de11228
molly <katsouli.mo@gmail.com>
2021-12-01 16:45:06 +0200

    Adds ability to create logger based on ctx_id in controller and a test

--- mipengine/controller/controller_logger.py
+++ mipengine/controller/controller_logger.py
@@ -1,11 +1,38 @@
 import logging
+from mipengine.controller import config as ctrl_config
 
 
-def getRequestLogger():
+def getRequestLogger(ctx_id=None):
     """
     Used for logging information produced after an endpoint request.
     """
-    return logging.getLogger("controller_context")
+    logger = (
+        initLogger(ctx_id)
+        if not logging.getLogger(ctx_id).hasHandlers()
+        else logging.getLogger(ctx_id)
+    )
+
+    return logger
+
+
+def initLogger(ctx_id):
+    logger = logging.getLogger(ctx_id)
+    if ctx_id == None:
+        formatter = logging.Formatter(f"%(message)s")
+    else:
+        # the name in the formatter is the name of the logger i.e. ctx_id
+        formatter = logging.Formatter(
+            f"%(asctime)s - %(levelname)s - CONTROLLER - ctx_id: %(name)s - %(message)s"
+        )
+    # StreamHandler
+    sh = logging.StreamHandler()
+    sh.setFormatter(formatter)
+    for hdlr in logger.handlers[:]:  # remove all old handlers
+        logger.removeHandler(hdlr)
+    logger.addHandler(sh)
+    logger.setLevel(ctrl_config.log_level)
+
+    return logger
 
 
 def getBackgroundServiceLogger():
