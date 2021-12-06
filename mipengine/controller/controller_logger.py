8a2f4ae0eb44510cae17d6b7d1fa827fc9ac6ca0
molly <katsouli.mo@gmail.com>
2021-12-06 15:59:08 +0200

    Adds controller logger

--- mipengine/controller/controller_logger.py
+++ mipengine/controller/controller_logger.py
@@ -2,35 +2,31 @@ import logging
 from mipengine.controller import config as ctrl_config
 
 
-def getRequestLogger(ctx_id=None):
+def getRequestLogger(context_id):
     """
     Used for logging information produced after an endpoint request.
     """
     logger = (
-        initLogger(ctx_id)
-        if not logging.getLogger(ctx_id).hasHandlers()
-        else logging.getLogger(ctx_id)
+        initLogger(context_id)
+        if not logging.getLogger(context_id).hasHandlers()
+        else logging.getLogger(context_id)
     )
 
     return logger
 
 
-def initLogger(ctx_id):
-    logger = logging.getLogger(ctx_id)
-    if ctx_id == None:
-        formatter = logging.Formatter(
-            f"%(asctime)s - %(levelname)s - CONTROLLER - ctx_id: %(name)s - %(message)s"
-        )
-    else:
-        # the name in the formatter is the name of the logger i.e. ctx_id
-        formatter = logging.Formatter(
-            f"%(asctime)s - %(levelname)s - CONTROLLER - ctx_id: %(name)s - %(message)s"
-        )
+def initLogger(context_id):
+    logger = logging.getLogger(context_id)
+    if logger.handlers:
+        return logger
+
+    formatter = logging.Formatter(
+        f"%(asctime)s - %(levelname)s - CONTROLLER - context_id: {context_id} - %(message)s"
+    )
+
     # StreamHandler
     sh = logging.StreamHandler()
     sh.setFormatter(formatter)
-    for hdlr in logger.handlers[:]:  # remove all old handlers
-        logger.removeHandler(hdlr)
     logger.addHandler(sh)
     logger.setLevel(ctrl_config.log_level)
 
