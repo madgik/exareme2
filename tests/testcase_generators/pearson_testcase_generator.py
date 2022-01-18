b4a7e2bb31d97eded921db0b78e419bc6a169329
Jason Sakellariou <js.sakel@gmail.com>
2022-01-18 16:03:14 +0200

    Add confidence level parameter in Pearson
    Also: regenerate test cases

--- tests/testcase_generators/pearson_testcase_generator.py
+++ tests/testcase_generators/pearson_testcase_generator.py
@@ -1,6 +1,8 @@
 from pathlib import Path
+import warnings
 
-from scipy.stats import pearsonr
+from scipy import stats
+import numpy as np
 
 from tests.testcase_generators.testcase_generator import TestCaseGenerator
 
@@ -26,17 +28,36 @@ class PearsonTestCaseGenerator(TestCaseGenerator):
         correlation_table["variables"] = list(X.columns)
         pvalues_table = {}
         pvalues_table["variables"] = list(X.columns)
+        low_ci_table = {}
+        low_ci_table["variables"] = list(X.columns)
+        high_ci_table = {}
+        high_ci_table["variables"] = list(X.columns)
         for y_col in Y.columns:
-            results = [pearsonr(Y[y_col], X[x_col]) for x_col in X.columns]
+            # correlations and p-values
+            results = [stats.pearsonr(Y[y_col], X[x_col]) for x_col in X.columns]
             correlations = [corr for corr, _ in results]
             pvalues = [pval for _, pval in results]
             correlation_table[y_col] = correlations
             pvalues_table[y_col] = pvalues
 
+            # confidence intervals
+            alpha = input_parameters["alpha"]
+            with warnings.catch_warnings():
+                warnings.simplefilter("ignore")
+                r_z = np.arctanh(correlations)
+            se = 1 / np.sqrt(n_obs - 3)
+            z = stats.norm.ppf(1 - alpha / 2)
+            lo_z, hi_z = r_z - z * se, r_z + z * se
+            lo, hi = np.tanh((lo_z, hi_z))
+            low_ci_table[y_col] = lo.tolist()
+            high_ci_table[y_col] = hi.tolist()
+
         return {
             "n_obs": n_obs,
             "correlations": correlation_table,
             "p-values": pvalues_table,
+            "low_confidence_intervals": low_ci_table,
+            "high_confidence_intervals": high_ci_table,
         }
 
 
