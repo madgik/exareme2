34bb5bb7e6bdbc790c22189583282841ece3c528
simos <simospm@di.uoa.gr>
2022-03-02 14:35:14 +0200

    small fix

--- tests/standalone_tests/test_cleanup_after_algorithm_execution.py
+++ tests/standalone_tests/test_cleanup_after_algorithm_execution.py
@@ -395,11 +395,6 @@ async def test_cleanup_rabbitmq_down_algorithm_execution(
     # give some time for node registry and cleanup background tasks to finish gracefully
     await asyncio.sleep(WAIT_BACKGROUND_TASKS_TO_FINISH)
 
-    # the node cervice was started in here so it must manually killed, otherwise it is
-    # alive through the whole pytest session and is erroneously accessed by other tests
-    # where teh node service is supposedly down
-    # kill_node_service(localnodetmp_node_service_proc)
-
     if (
         globalnode_tables_before_cleanup
         and not globalnode_tables_after_cleanup
@@ -571,7 +566,7 @@ async def test_cleanup_node_service_down_algorithm_execution(
     # the node cervice was started in here so it must manually killed, otherwise it is
     # alive through the whole pytest session and is erroneously accessed by other tests
     # where teh node service is supposedly down
-    kill_localnodetmp_node_service(localnodetmp_node_service_proc)
+    kill_node_service(localnodetmp_node_service_proc)
 
     if (
         globalnode_tables_before_cleanup
