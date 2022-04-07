7a0f323530576e8fc0abb50d1be20cff8d2d9547
KFilippopolitis <kostasfilippop@gmail.com>
2022-04-07 04:14:17 -0700

    Fix on test_get_datasets_location

--- tests/prod_env_tests/test_get_datasets_location.py
+++ tests/prod_env_tests/test_get_datasets_location.py
@@ -7,59 +7,59 @@ from tests.prod_env_tests import datasets_location_url
 
 
 @pytest.fixture
-def expected_datasets_location():
+def expected_datasets_per_data_model():
     return {
         "dementia:0.1": {
-            "desd-synthdata0": ["localnode1"],
-            "desd-synthdata1": ["localnode2"],
-            "desd-synthdata2": ["localnode2"],
-            "desd-synthdata3": ["localnode2"],
-            "desd-synthdata4": ["localnode2"],
-            "desd-synthdata5": ["localnode2"],
-            "desd-synthdata6": ["localnode2"],
-            "desd-synthdata7": ["localnode2"],
-            "desd-synthdata8": ["localnode2"],
-            "desd-synthdata9": ["localnode2"],
-            "edsd0": ["localnode1"],
-            "edsd1": ["localnode2"],
-            "edsd2": ["localnode2"],
-            "edsd3": ["localnode2"],
-            "edsd4": ["localnode2"],
-            "edsd5": ["localnode2"],
-            "edsd6": ["localnode2"],
-            "edsd7": ["localnode2"],
-            "edsd8": ["localnode2"],
-            "edsd9": ["localnode2"],
-            "ppmi0": ["localnode1"],
-            "ppmi1": ["localnode2"],
-            "ppmi2": ["localnode2"],
-            "ppmi3": ["localnode2"],
-            "ppmi4": ["localnode2"],
-            "ppmi5": ["localnode2"],
-            "ppmi6": ["localnode2"],
-            "ppmi7": ["localnode2"],
-            "ppmi8": ["localnode2"],
-            "ppmi9": ["localnode2"],
+            "desd-synthdata0",
+            "desd-synthdata1",
+            "desd-synthdata2",
+            "desd-synthdata3",
+            "desd-synthdata4",
+            "desd-synthdata5",
+            "desd-synthdata6",
+            "desd-synthdata7",
+            "desd-synthdata8",
+            "desd-synthdata9",
+            "edsd0",
+            "edsd1",
+            "edsd2",
+            "edsd3",
+            "edsd4",
+            "edsd5",
+            "edsd6",
+            "edsd7",
+            "edsd8",
+            "edsd9",
+            "ppmi0",
+            "ppmi1",
+            "ppmi2",
+            "ppmi3",
+            "ppmi4",
+            "ppmi5",
+            "ppmi6",
+            "ppmi7",
+            "ppmi8",
+            "ppmi9",
         },
         "tbi:0.1": {
-            "dummy_tbi0": ["localnode1"],
-            "dummy_tbi1": ["localnode2"],
-            "dummy_tbi2": ["localnode2"],
-            "dummy_tbi3": ["localnode2"],
-            "dummy_tbi4": ["localnode2"],
-            "dummy_tbi5": ["localnode2"],
-            "dummy_tbi6": ["localnode2"],
-            "dummy_tbi7": ["localnode2"],
-            "dummy_tbi8": ["localnode2"],
-            "dummy_tbi9": ["localnode2"],
+            "dummy_tbi0",
+            "dummy_tbi1",
+            "dummy_tbi2",
+            "dummy_tbi3",
+            "dummy_tbi4",
+            "dummy_tbi5",
+            "dummy_tbi6",
+            "dummy_tbi7",
+            "dummy_tbi8",
+            "dummy_tbi9",
         },
     }
 
 
-def test_get_datasets_location(expected_datasets_location):
+def test_get_datasets_location(expected_datasets_per_data_model):
     request = requests.get(datasets_location_url)
     response = json.loads(request.text)
     for data_model in response:
-        assert data_model in expected_datasets_location
+        assert data_model in expected_datasets_per_data_model
         for dataset in response[data_model]:
-            assert dataset in expected_datasets_location[data_model]
+            assert dataset in expected_datasets_per_data_model[data_model]
