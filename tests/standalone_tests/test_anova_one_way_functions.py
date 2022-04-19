0ac6c8eafe8e79ae8da21da822a46b418e42cd79
molly <katsouli.mo@gmail.com>
2022-04-19 14:25:04 +0300

    Fixes independent function test

--- tests/standalone_tests/test_anova_one_way_functions.py
+++ tests/standalone_tests/test_anova_one_way_functions.py
@@ -8,7 +8,16 @@ def test_min_max_ci():
     sample_stds = [1, 2, 1, 1]
     categories = ["A", "B", "C", "D"]
     group_stats_index = ["B", "A", "C", "D"]
-    ci_info = get_min_max_ci_info(means, sample_stds, categories, group_stats_index)
+    var_min_per_group = [1, 2, 3, 4]
+    var_max_per_group = [7, 8, 9, 10]
+    min_max_dict, ci_info = get_min_max_ci_info(
+        means,
+        sample_stds,
+        categories,
+        var_min_per_group,
+        var_max_per_group,
+        group_stats_index,
+    )
     ci_info_exp = {
         "m-s": {"A": 1, "B": 1, "C": 3, "D": 4},
         "m+s": {"A": 3, "B": 5, "C": 5, "D": 6},
