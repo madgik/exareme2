2c814f4e13b9b92d297911b74ac9edc7deb8df6e
molly <katsouli.mo@gmail.com>
2022-06-16 11:54:23 +0300

    Adds paired t-test

---
+++ mipengine/algorithms/paired_ttest.py
@@ -0,0 +1,162 @@
+import json
+from typing import TypeVar
+
+import numpy
+import numpy as np
+from pydantic import BaseModel
+
+from mipengine.udfgen import secure_transfer
+from mipengine.udfgen.udfgenerator import literal
+from mipengine.udfgen.udfgenerator import relation
+from mipengine.udfgen.udfgenerator import transfer
+from mipengine.udfgen.udfgenerator import udf
+
+
+class TtestResult(BaseModel):
+    t_stat: float
+    df: int
+    p: float
+    mean_diff: float
+    se_diff: float
+    ci_upper: float
+    ci_lower: float
+    cohens_d: float
+
+
+def run(algo_interface):
+    local_run = algo_interface.run_udf_on_local_nodes
+    global_run = algo_interface.run_udf_on_global_node
+    alpha = algo_interface.algorithm_parameters["alpha"]
+    alternative = algo_interface.algorithm_parameters["alt_hypothesis"]
+
+    X_relation, Y_relation = algo_interface.create_primary_data_views(
+        variable_groups=[algo_interface.x_variables, algo_interface.y_variables],
+    )
+
+    [x_var_name] = algo_interface.x_variables
+    [y_var_name] = algo_interface.y_variables
+
+    sec_local_transfer = local_run(
+        func=local_paired,
+        keyword_args=dict(y=Y_relation, x=X_relation),
+        share_to_global=[True],
+    )
+
+    result = global_run(
+        func=global_paired,
+        keyword_args=dict(
+            sec_local_transfer=sec_local_transfer, alpha=alpha, alternative=alternative
+        ),
+    )
+
+    result = json.loads(result.get_table_data()[1][0])
+    res = TtestResult(
+        t_stat=result["t_stat"],
+        df=result["df"],
+        p=result["p"],
+        mean_diff=result["mean_diff"],
+        se_diff=result["se_diff"],
+        ci_upper=result["ci_upper"],
+        ci_lower=result["ci_lower"],
+        cohens_d=result["cohens_d"],
+    )
+
+    return res
+
+
+S = TypeVar("S")
+T = TypeVar("T")
+
+
+@udf(
+    y=relation(schema=T),
+    x=relation(schema=S),
+    return_type=[secure_transfer(sum_op=True)],
+)
+def local_paired(x, y):
+    x1 = x.reset_index(drop=True).to_numpy().squeeze()
+    x2 = y.reset_index(drop=True).to_numpy().squeeze()
+    x1_sum = sum(x1)
+    x2_sum = sum(x2)
+    n_obs = len(x)
+    diff = sum(x1 - x2)
+    diff_sqrd = sum((x1 - x2) ** 2)
+    x1_sqrd_sum = sum(x1**2)
+    x2_sqrd_sum = sum(x2**2)
+
+    sec_transfer_ = {
+        "n_obs": {"data": n_obs, "operation": "sum"},
+        "sum_x1": {"data": x1_sum.item(), "operation": "sum"},
+        "sum_x2": {"data": x2_sum.item(), "operation": "sum"},
+        "diff": {"data": diff.tolist(), "operation": "sum"},
+        "diff_sqrd": {"data": diff_sqrd.tolist(), "operation": "sum"},
+        "x1_sqrd_sum": {"data": x1_sqrd_sum.tolist(), "operation": "sum"},
+        "x2_sqrd_sum": {"data": x2_sqrd_sum.tolist(), "operation": "sum"},
+    }
+
+    return sec_transfer_
+
+
+@udf(
+    sec_local_transfer=secure_transfer(sum_op=True),
+    alpha=literal(),
+    alternative=literal(),
+    return_type=[transfer()],
+)
+def global_paired(sec_local_transfer, alpha, alternative):
+    from scipy.stats import t
+
+    n_obs = sec_local_transfer["n_obs"]
+    sum_x1 = sec_local_transfer["sum_x1"]
+    sum_x2 = sec_local_transfer["sum_x2"]
+    diff_sum = sec_local_transfer["diff"]
+    diff_sqrd_sum = sec_local_transfer["diff_sqrd"]
+    x1_sqrd_sum = sec_local_transfer["x1_sqrd_sum"]
+    x2_sqrd_sum = sec_local_transfer["x2_sqrd_sum"]
+
+    mean_x1 = sum_x1 / n_obs
+    mean_x2 = sum_x2 / n_obs
+    devel_x1 = x1_sqrd_sum - 2 * mean_x1 * sum_x1 + (mean_x1**2) * n_obs
+    devel_x2 = x2_sqrd_sum - 2 * sum_x2 * mean_x2 + (mean_x2**2) * n_obs
+    sd_x1 = numpy.sqrt(devel_x1 / (n_obs - 1))
+    sd_x2 = numpy.sqrt(devel_x2 / (n_obs - 1))
+
+    # standard deviation of the difference between means
+    sd = numpy.sqrt((diff_sqrd_sum - (diff_sum**2 / n_obs)) / (n_obs - 1))
+
+    # standard error of the difference between means
+    sed = sd / numpy.sqrt(n_obs)
+
+    # t-statistic
+    t_stat = (mean_x1 - mean_x2) / sed
+    df = n_obs - 1
+
+    # p-value for alternative = 'greater'
+    if alternative == "greater":
+        p = 1.0 - t.cdf(t_stat, df)
+    # p-value for alternative = 'less'
+    elif alternative == "less":
+        p = 1.0 - t.cdf(-t_stat, df)
+    # p-value for alternative = 'two-sided'
+    else:
+        p = (1.0 - t.cdf(abs(t_stat), df)) * 2.0
+
+    # Confidence intervals
+    sample_mean = diff_sum / n_obs
+    ci = t.interval(alpha=1 - alpha, df=n_obs - 1, loc=sample_mean, scale=sed)
+
+    # Cohen’s d
+    cohens_d = (mean_x1 - mean_x2) / numpy.sqrt((sd_x1**2 + sd_x2**2) / 2)
+
+    transfer_ = {
+        "t_stat": t_stat,
+        "df": df,
+        "p": p,
+        "mean_diff": diff_sum,
+        "se_diff": sed,
+        "ci_upper": ci[1],
+        "ci_lower": ci[0],
+        "cohens_d": cohens_d,
+    }
+
+    return transfer_
