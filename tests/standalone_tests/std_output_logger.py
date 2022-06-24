e6b5a4d36c5c2d7db815e359ba2e747c91077200
simos <simospm@di.uoa.gr>
2022-06-24 14:47:08 +0300

    fix and color

--- tests/standalone_tests/std_output_logger.py
+++ tests/standalone_tests/std_output_logger.py
@@ -1,9 +1,28 @@
+from termcolor import colored
+
+
 class StdOutputLogger:
-    def info(msg: str, *args, **kwargs):
+    def info(self, msg: str, *args, **kwargs):
+        prefix = colored("INFO", "white", "on_green")
+        print(f"{prefix} - ", end="")
         print(msg)
 
-    def debug(msg: str, *args, **kwargs):
+    def debug(self, msg: str, *args, **kwargs):
+        prefix = colored("DEBUG", "white", "on_purple")
+        print(f"{prefix} - ", end="")
         print(msg)
 
-    def error(msg: str, *args, **kwargs):
+    def warning(self, msg: str, *args, **kwargs):
+        prefix = colored("ERROR", "white", "on_yellow")
+        print(f"{prefix} - ", end="")
+        print(msg)
+
+    def error(self, msg: str, *args, **kwargs):
+        prefix = colored("ERROR", "white", "on_red")
+        print(f"{prefix} - ", end="")
+        print(msg)
+
+    def critical(self, msg: str, *args, **kwargs):
+        prefix = colored("ERROR", "red")
+        print(f"{prefix} - ", end="")
         print(msg)
