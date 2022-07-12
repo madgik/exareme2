75d694662d2535d74dd86982231c7c0213322640
Apostolos Glenis <apostglen46@gmail.com>
2022-07-12 17:01:08 +0300

    added first test for descriptive statistics

---
+++ tests/algorithm_validation_tests/test_descriptive_statistics.py
@@ -0,0 +1,44 @@
+import json
+from pathlib import Path
+
+import numpy as np
+import pytest
+import requests
+
+expected_file = Path(__file__).parent / "expected" / "descriptive_statistics_expected.json"
+
+
+def desc_request(input):
+    url = "http://127.0.0.1:5000/algorithms" + "/descriptive_statistics"
+
+    filters = None
+    input["inputdata"]["filters"] = filters
+    request_json = json.dumps(input)
+
+    headers = {"Content-type": "application/json", "Accept": "text/plain"}
+    response = requests.post(url, data=request_json, headers=headers)
+    return response
+
+
+def get_test_params(expected_file, slc=None):
+    with expected_file.open() as f:
+        params = json.load(f)["test_cases"]
+    if not slc:
+        slc = slice(len(params))
+    params = [(p["input"], p["output"]) for p in params[slc]]
+    return params
+
+
+@pytest.mark.parametrize("test_input, expected", get_test_params(expected_file))
+def test_descriptive_algorithm(test_input, expected):
+    response = desc_request(test_input)
+    result = json.loads(response.content)
+
+    assert response.status_code == 200
+    assert result["categorical_counts"] == expected["categorical_counts"]
+    np.testing.assert_allclose(
+        result["max_model"],
+        expected["max_model"],
+        rtol=1e-7,
+        atol=1e-10,
+    )
