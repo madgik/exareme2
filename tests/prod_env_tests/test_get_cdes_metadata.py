f8dbd303d046922968fde662ac8577cf9d38be2e
Jason Sakellariou <js.sakel@gmail.com>
2022-07-23 10:44:45 +0300

    dementia CDEs: remove variable used in old logistic version

--- tests/prod_env_tests/test_get_cdes_metadata.py
+++ tests/prod_env_tests/test_get_cdes_metadata.py
@@ -1,23 +1,15 @@
 import json
 
-import pytest
 import requests
 
 from tests.prod_env_tests import cdes_metadata_url
 
 
-@pytest.fixture
-def expected_data_model_len_metadata():
-    return {
-        "dementia:0.1": 186,
-        "tbi:0.1": 21,
-    }
-
-
-def test_get_cdes_metadata(expected_data_model_len_metadata):
+def test_get_cdes_metadata():
     request = requests.get(cdes_metadata_url)
     response = json.loads(request.text)
 
     for data_model, cdes in response.items():
-        assert data_model in expected_data_model_len_metadata
-        assert len(cdes) == expected_data_model_len_metadata[data_model]
+        assert data_model in ["dementia:0.1", "tbi:0.1"]
+        assert len(cdes) > 0
+        assert "dataset" in cdes
