8d77b91dbe0de6492375fa90bb1a3f4ef185f495
Kfilippopolitis <kostasfilippop@gmail.com>
2022-07-29 15:28:57 +0300

    Minor adjustments to endpoints.

--- mipengine/controller/api/algorithms_endpoint.py
+++ mipengine/controller/api/algorithms_endpoint.py
@@ -25,13 +25,18 @@ async def get_datasets() -> dict:
 
 @algorithms.route("/datasets_locations", methods=["GET"])
 async def get_datasets_locations() -> dict:
-    return controller.get_datasets_locations().datasets_locations
+    return {
+        data_model: datasets_locations
+        for data_model, datasets_locations in controller.get_datasets_locations().datasets_locations.items()
+    }
 
 
 @algorithms.route("/cdes_metadata", methods=["GET"])
 async def get_cdes_metadata() -> dict:
     return {
-        data_model: cdes.dict()
+        data_model: {
+            column: metadata.dict() for column, metadata in cdes.values.items()
+        }
         for data_model, cdes in controller.get_cdes_per_data_model().data_models_cdes.items()
     }
 
