e076d46cf72c48e1a5fdee9551407e5355d51d53
K.Filippopolitis <56073635+KFilippopolitis@users.noreply.github.com>
2022-09-13 11:12:39 +0300

    Dev/mip 654/max connections as env variable (#280)
    * Nclients is now an environmental variable:
    1. bootstrap.sh now handles properly a docker restart on the monetdb container.
    2. kubernetes config now contains the nclients variable too.

--- monetdb/bootstrap.sh
+++ monetdb/bootstrap.sh
@@ -1,15 +1,28 @@
 #!/usr/bin/env bash
+monetdbd get all /home/monetdb > /dev/null 2>&1
+EXIT_STATUS=$?
 
-# Create the monetdb daemon
-monetdbd create $MONETDB_STORAGE
-monetdbd set port=50000 $MONETDB_STORAGE
-monetdbd set listenaddr=0.0.0.0  $MONETDB_STORAGE
-monetdbd start $MONETDB_STORAGE
+if [ "$EXIT_STATUS" -eq 1 ]
+then
+  echo 'Initializing database'
 
-# Create the database instance
-monetdb create db
-monetdb set nclients=128 db
-monetdb set embedpy3=true db
-monetdb release db
+  # Create the monetdb daemon
+  monetdbd create $MONETDB_STORAGE
+  monetdbd set port=50000 $MONETDB_STORAGE
+  monetdbd set listenaddr=0.0.0.0  $MONETDB_STORAGE
+  monetdbd start $MONETDB_STORAGE
+
+  # Create the database instance
+  monetdb create db
+  monetdb set nclients=$MONETDB_NCLIENTS db
+  monetdb set embedpy3=true db
+  monetdb release db
+  echo 'Database initialized'
+else
+  monetdbd start $MONETDB_STORAGE
+  monetdb set nclients=$MONETDB_NCLIENTS db
+  echo "Number of clients were set"
+  monetdb start db
+fi
 
 tail -fn +1 $MONETDB_STORAGE/merovingian.log
