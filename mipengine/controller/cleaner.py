53d0bf5feba25f8fff4293d96a970d9ad726302a
simos <simospm@di.uoa.gr>
2022-09-23 17:01:35 +0300

    Cleaner logic changed from storing multiple cleanup entries in 1 file to creating multiple files each storing a single cleanup entry, thus removing heavy complexity of locking mechansims

--- mipengine/controller/cleaner.py
+++ mipengine/controller/cleaner.py
@@ -1,10 +1,11 @@
 import os
 import threading
 import time
+import traceback
 from datetime import datetime
 from datetime import timezone
 from pathlib import Path
-from threading import Lock
+from threading import RLock
 from typing import List
 
 import toml
@@ -19,11 +20,12 @@ from mipengine.controller.node_landscape_aggregator import NodeLandscapeAggregat
 from mipengine.singleton import Singleton
 
 CLEANER_REQUEST_ID = "CLEANER"
+CLEANUP_FILE_PREFIX = "cleanup"
 CONTEXT_ID_CLEANUP_FILE = "contextids_cleanup.toml"
 
 # Cleanup entry example:
-# [3502300] <--context_id
-# nodes = [ "testglobalnode", "testlocalnode1", "testlocalnodetmp",]
+# context_id= "3502300"
+# node_ids = [ "testglobalnode", "testlocalnode1", "testlocalnode2",]
 # timestamp = "2022-05-23T14:40:34.203085+00:00"
 # released = true
 
@@ -39,12 +41,26 @@ class _NodeInfoDTO(BaseModel):
         allow_mutation = False
 
 
+class _CleanupEntry(BaseModel):
+    context_id: str
+    node_ids: List[str]
+    timestamp: str
+    released: bool
+
+
+def _is_timestamp_expired(timestamp: str):
+    now = datetime.now(timezone.utc)
+    timestamp = datetime.fromisoformat(timestamp)
+    time_elapsed = now - timestamp
+    return time_elapsed.seconds > controller_config.cleanup.contextid_release_timelimit
+
+
 class Cleaner(metaclass=Singleton):
     def __init__(self):
         self._logger = ctrl_logger.get_background_service_logger()
 
-        self._cleanup_file_processor = CleanupFileProcessor(self._logger)
-        self._clean_up_interval = controller_config.cleanup.nodes_cleanup_interval
+        self._cleanup_files_processor = CleanupFilesProcessor(self._logger)
+        self._cleanup_interval = controller_config.cleanup.nodes_cleanup_interval
 
         self._keep_cleaning_up = True
         self._cleanup_loop_thread = None
@@ -52,50 +68,88 @@ class Cleaner(metaclass=Singleton):
     def _cleanup_loop(self):
         while self._keep_cleaning_up:
             try:
-                contextids_and_status = self._cleanup_file_processor.read_cleanup_file()
-                for context_id, status in contextids_and_status.items():
-                    self._process_contextid(context_id, status)
+                all_entries = self._cleanup_files_processor.get_all_entries() or []
+                for entry in all_entries:
+                    if entry.released or _is_timestamp_expired(entry.timestamp):
+                        failed_node_ids = self._exec_context_id_cleanup(
+                            entry.context_id, entry.node_ids
+                        )
+                        if failed_node_ids:
+                            self._logger.debug(
+                                f"'Altering' file with {entry.context_id=}, removing node ids "
+                                "wich succeded cleanup, keeping only failed node ids"
+                            )
+                            entry.node_ids = failed_node_ids
+                            self._logger.debug(
+                                f"Deleting file with {entry.context_id=}"
+                            )
+                            self._cleanup_files_processor.delete_file_by_context_id(
+                                entry.context_id
+                            )
+                            self._logger.debug(
+                                f"Creating file with {entry.context_id=}"
+                            )
+                            self._cleanup_files_processor.create_file_from_cleanup_entry(
+                                entry
+                            )
+
+                        else:
+                            self._logger.debug(
+                                f"Cleanup for {entry.context_id=} complete. Deleting file"
+                            )
+                            self._cleanup_files_processor.delete_file_by_context_id(
+                                entry.context_id
+                            )
+
             except Exception as exc:
-                self._logger.error(f"Cleanup exception: {type(exc)}:{exc}")
+                self._logger.error(traceback.format_exc())
+
             finally:
-                time.sleep(self._clean_up_interval)
+                time.sleep(self._cleanup_interval)
 
-    def _process_contextid(self, context_id: str, status: dict):
-        if status["nodes"]:
-            if status["released"] or self._is_expired(status["timestamp"]):
-                self._cleanup_nodes_list(
-                    context_id=context_id, node_ids=status["nodes"]
-                )
-        else:
-            self._remove_contextid_from_cleanup(context_id=context_id)
-
-    def _is_expired(self, timestamp: str):
-        now = datetime.now(timezone.utc)
-        timestamp = datetime.fromisoformat(timestamp)
-        time_elapsed = now - timestamp
-        return (
-            time_elapsed.seconds > controller_config.cleanup.contextid_release_timelimit
-        )
-
-    def _cleanup_nodes_list(self, context_id: str, node_ids: List[str]):
+    def _exec_context_id_cleanup(
+        self, context_id: str, node_ids: [str]
+    ) -> List[str]:  # returns failed node_ids
+        node_task_handlers_to_async_results = {}
         for node_id in node_ids:
             try:
                 node_info = self._get_node_info_by_id(node_id)
-                task_handler = _create_node_task_handler(node_info)
-                task_handler.clean_up(
-                    request_id=CLEANER_REQUEST_ID,
-                    context_id=context_id,
-                )
-                self._remove_nodeid_from_cleanup(context_id=context_id, node_id=node_id)
+            except Exception as exc:
                 self._logger.debug(
-                    f"clean_up task succeeded for {node_id=} for {context_id=}"
+                    f"Could not get node info for {node_id=}. The node is "
+                    "most likely offline"
+                )
+                continue
+            task_handler = _get_node_task_handler(node_info)
+
+            node_task_handlers_to_async_results[
+                task_handler
+            ] = task_handler.queue_cleanup(
+                request_id=CLEANER_REQUEST_ID,
+                context_id=context_id,
+            )
+
+        failed_node_ids = []
+        for task_handler, async_result in node_task_handlers_to_async_results.items():
+            try:
+                task_handler.wait_queued_cleanup_complete(
+                    async_result=async_result,
+                    request_id=CLEANER_REQUEST_ID,
                 )
             except Exception as exc:
-                self._logger.error(
-                    f"clean_up task FAILED for {node_id=} "
-                    f"for {context_id=}. Will retry in {self._clean_up_interval=} secs. Fail "
-                    f"reason: {type(exc)}:{exc}"
+                failed_node_ids.append(task_handler.node_id)
+                self._logger.warning(
+                    f"Cleanup task for {task_handler.node_id=}, for {context_id=} FAILED. "
+                    f"Will retry in {self._cleanup_interval=} secs. Failure occured while "
+                    "waiting the completion of the task (wait_queued_cleanup_complete), "
+                    f"the exception raised was: {type(exc)}:{exc}"
                 )
+                continue
+
+            self._logger.debug(
+                f"Cleanup task succeeded for {task_handler.node_id=} for {context_id=}"
+            )
+        return failed_node_ids
 
     def start(self):
         self.stop()
@@ -114,30 +168,25 @@ class Cleaner(metaclass=Singleton):
     def add_contextid_for_cleanup(
         self, context_id: str, algo_execution_node_ids: List[str]
     ):
-        self._cleanup_file_processor.append_to_cleanup_file(
-            context_id=context_id, node_ids=algo_execution_node_ids
+        self._logger.debug(f"Creating file for new {context_id=}")
+        now_timestamp = datetime.now(timezone.utc).isoformat()
+        entry = _CleanupEntry(
+            context_id=context_id,
+            node_ids=algo_execution_node_ids,
+            timestamp=now_timestamp,
+            released=False,
         )
+        self._cleanup_files_processor.create_file_from_cleanup_entry(entry)
 
-    def _remove_contextid_from_cleanup(self, context_id: str):
-        self._cleanup_file_processor.remove_from_cleanup_file(context_id=context_id)
-
-    def _remove_nodeid_from_cleanup(self, context_id: str, node_id: str):
-        self._cleanup_file_processor.remove_from_cleanup_file(
-            context_id=context_id, node_id=node_id
-        )
-
-    def release_contextid_for_cleanup(self, context_id: str):
-        self._cleanup_file_processor.set_released_true_to_file(context_id=context_id)
+    def release_context_id(self, context_id):
+        self._logger.debug(f"Setting released to true for file with {context_id=}")
+        entry = self._cleanup_files_processor.get_entry_by_context_id(context_id)
+        entry.released = True
+        self._cleanup_files_processor.delete_file_by_context_id(context_id)  # by entry?
+        self._cleanup_files_processor.create_file_from_cleanup_entry(entry)
 
     def _get_node_info_by_id(self, node_id: str) -> _NodeInfoDTO:
-        try:
-            node_info = NodeLandscapeAggregator().get_node_info(node_id)
-        except KeyError as exc:
-            raise KeyError(
-                f"(Cleaner::_get_node_info_by_id) Node with id '{node_id}' is "
-                f"not currently available. Exception: {exc}"
-            )
-
+        node_info = NodeLandscapeAggregator().get_node_info(node_id)
         return _NodeInfoDTO(
             node_id=node_info.id,
             queue_address=":".join([str(node_info.ip), str(node_info.port)]),
@@ -147,136 +196,93 @@ class Cleaner(metaclass=Singleton):
         )
 
     # This is only supposed to be called from a test.
-    # In all other circumstances cleanup should not be reset manually
-    def _reset_cleanup(self):
-        self._cleanup_file_processor._initialize_cleanup_file()
+    # In all other circumstances the cleaner should not be reset manually
+    def _reset(self):
+        self._cleanup_files_processor._delete_all_entries()
 
 
-def _create_node_task_handler(node_info: _NodeInfoDTO) -> NodeAlgorithmTasksHandler:
+def _get_node_task_handler(node_info: _NodeInfoDTO) -> NodeAlgorithmTasksHandler:
     return NodeAlgorithmTasksHandler(
         node_id=node_info.node_id,
         node_queue_addr=node_info.queue_address,
         node_db_addr=node_info.db_address,
-        tasks_timeout=node_info.tasks_timeout,
+        tasks_timeout=2,  # node_info.tasks_timeout,
         run_udf_task_timeout=node_info.run_udf_task_timeout,
     )
 
 
-class CleanupFileProcessor:
+class CleanupFilesProcessor:
     def __init__(self, logger):
-        self._file_lock = Lock()
+        self._lock = RLock()
         self._logger = logger
-
-        # Create all parent folders, if needed.
-        Path(controller_config.cleanup.contextids_cleanup_folder).mkdir(
-            parents=True, exist_ok=True
-        )
-
-        self._cleanup_file_path = Path(
+        self._cleanup_entries_folder_path = Path(
             controller_config.cleanup.contextids_cleanup_folder
-        ).joinpath(Path(CONTEXT_ID_CLEANUP_FILE))
-
-        # create file if it does not exist
-        if not os.path.isfile(self._cleanup_file_path):
-            self._initialize_cleanup_file()
-
-        # changes to the file will be first written on a temporary file. Then the temporary
-        # file replaces the cleanup file. The reason for that is that renaming is atomic
-        # (under linux) so the chances of the cleanup file to get corrupted is minimized
-        dirname = os.path.dirname(self._cleanup_file_path)
-        filename_tmp = (
-            Path(self._cleanup_file_path).stem
-            + "_tmp"
-            + Path(self._cleanup_file_path).suffix
         )
-        self._cleanup_file_tmp_path = os.path.join(dirname, filename_tmp)
 
-    def append_to_cleanup_file(self, context_id: str, node_ids: List[str]):
-        parsed_toml = self.read_cleanup_file()
-        if context_id not in parsed_toml:
-            parsed_toml[context_id] = {"nodes": node_ids}
-        else:
-            self._logger.warning(
-                f"(Cleaner::append_to_cleanup_file) Attempting to add {context_id=} for "
-                f"cleanup but this context_id is already in the contextids_cleanup_file. "
-                f"This should never happen..."
-            )
-            parsed_toml[context_id]["nodes"].extend(node_ids)
-            # remove possible duplicates
-            parsed_toml[context_id]["nodes"] = list(
-                set(parsed_toml[context_id]["nodes"])
-            )
+        # Create the folder, if does not exist.
+        self._cleanup_entries_folder_path.mkdir(parents=True, exist_ok=True)
 
-        now_timestamp = datetime.now(timezone.utc)
-        parsed_toml[context_id]["timestamp"] = now_timestamp.isoformat()
-        parsed_toml[context_id]["released"] = False
+    def create_file_from_cleanup_entry(self, entry: _CleanupEntry):
+        entry_dict = entry.dict()
+        filename = f"{CLEANUP_FILE_PREFIX}_{entry.context_id}.toml"
+        full_path_and_filename = os.path.join(
+            self._cleanup_entries_folder_path, filename
+        )
+        with open(full_path_and_filename, "x") as f:
+            toml.dump(entry_dict, f)
+        self._logger.debug(f"Created file with {entry.context_id=}")
 
-        self._write_to_cleanup_file(parsed_toml)
-
-    def set_released_true_to_file(self, context_id: str):
-        parsed_toml = self.read_cleanup_file()
-        if context_id in parsed_toml:
-            parsed_toml[context_id]["released"] = True
-
-        self._write_to_cleanup_file(parsed_toml)
-
-    def remove_from_cleanup_file(self, context_id: str, node_id: str = None):
-        parsed_toml = self.read_cleanup_file()
-        if context_id in parsed_toml:
-            if node_id:
-                try:
-                    parsed_toml[context_id]["nodes"].remove(node_id)
-                except ValueError:
-                    self._logger.warning(
-                        f"(Cleaner::remove_from_cleanup_file) Tried to remove {node_id=} "
-                        f"for {context_id=} but this context_id.node_id is not in the "
-                        f"clean_up file.This should not happen."
-                    )
-                    pass
-            else:
-                parsed_toml.pop(context_id)
-        else:
-            self._logger.warning(
-                f"(Cleaner::remove_from_cleanup_file) Tried to remove {context_id=} but "
-                f"this context_id is not in the clean_up file.This should not happen."
-            )
-            pass
-
-        self._write_to_cleanup_file(parsed_toml)
-
-    def read_cleanup_file(self) -> dict:
-        with self._file_lock:
-            if not os.path.isfile(self._cleanup_file_path):
+    def get_all_entries(self) -> [_CleanupEntry]:
+        cleanup_entries = []
+        for _file in self._cleanup_entries_folder_path.glob("cleanup_*.toml"):
+            try:
+                parsed_toml = toml.load(_file)
+            except Exception as exc:
                 self._logger.warning(
-                    f"(Cleaner::read_cleanup_file) {self._cleanup_file_path=} does not "
-                    f"exist. This should not happen"
+                    f"Trying to read {_file.name=} raised exception: {exc}"
                 )
-                return {}
-            with open(self._cleanup_file_path, "r") as f:
+
+            cleanup_entries.append(_CleanupEntry(**parsed_toml))
+        return cleanup_entries
+
+    def delete_file_by_context_id(self, context_id: str):
+        try:
+            self._get_file_by_context_id(context_id).unlink()
+        except FileNotFoundError as exc:
+            self._logger.warning(
+                f"Tried to delete {_file=} but file does not "
+                f"exist. This should not happen. \n{exc=}"
+            )
+        self._logger.debug(f"Deleted file with {context_id=}")
+
+    def get_entry_by_context_id(self, context_id) -> _CleanupEntry:
+        _file = self._get_file_by_context_id(context_id)
+        if _file:
+            try:
+                parsed_toml = toml.load(_file)
+            except Exception as exc:
+                self._logger.warning(
+                    f"Trying to read {_file.name=} raised exception: {exc}"
+                )
+            return _CleanupEntry(**parsed_toml)
+        else:
+            self._logger.warning(
+                f"Could not find entry and cleanup file with {context_id=}"
+            )
+
+    def _get_file_by_context_id(self, context_id: str):  # return type??
+        for _file in self._cleanup_entries_folder_path.glob("cleanup_*.toml"):
+            if context_id in _file.name:  # edge case
                 try:
-                    parsed_toml = toml.load(f)
+                    parsed_toml = toml.load(_file)
                 except Exception as exc:
                     self._logger.warning(
-                        f"(Cleaner::read_cleanup_file) Trying to read {controller_config.cleanup.contextids_cleanup_file=} "
-                        f"raised exception: {exc}"
+                        f"Trying to read {_file.name=} raised exception: {exc}"
                     )
-            return parsed_toml
+                if parsed_toml["context_id"] == context_id:
+                    return _file
+        self._logger.warning(f"Cleanup file with {context_id=} could not be found")
 
-    def _write_to_cleanup_file(self, toml_string: str):
-        with self._file_lock:
-            with open(self._cleanup_file_tmp_path, "w") as f:
-                toml.dump(toml_string, f)
-
-            # renaming is atomic. Of course if more Controllers are spawn the file is very
-            # likely to become corrupted
-            os.rename(self._cleanup_file_tmp_path, self._cleanup_file_path)
-
-    def _initialize_cleanup_file(self):
-        with self._file_lock:
-            # delete it
-            cleanup_file_path = Path(
-                controller_config.cleanup.contextids_cleanup_folder
-            ).joinpath(Path(CONTEXT_ID_CLEANUP_FILE))
-            cleanup_file_path.unlink(missing_ok=True)
-            # create it
-            Path(self._cleanup_file_path).touch()
+    def _delete_all_entries(self):
+        for _file in self._cleanup_entries_folder_path.glob("cleanup_*.toml"):
+            _file.unlink()
