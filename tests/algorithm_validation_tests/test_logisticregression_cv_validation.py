afc2bc6cba129c22c554ba1335c70afe6e9ccb49
Jason Sakellariou <js.sakel@gmail.com>
2022-09-29 16:08:54 +0300

    Logistic regression cross-validation: tests
    https://team-1617704806227.atlassian.net/browse/MIP-642

---
+++ tests/algorithm_validation_tests/test_logisticregression_cv_validation.py
@@ -0,0 +1,80 @@
+import json
+from pathlib import Path
+
+import pytest
+import scipy.stats as st
+
+from tests.algorithm_validation_tests.helpers import algorithm_request
+from tests.algorithm_validation_tests.helpers import get_test_params
+
+expected_file = (
+    Path(__file__).parent / "expected" / "logisticregression_cv_expected.json"
+)
+
+
+@pytest.mark.parametrize(
+    "test_input, expected",
+    get_test_params(
+        expected_file,
+        skip_indices=[5, 6, 9, 12, 18, 19, 22],
+        skip_reason="Run results in empty tables,"
+        " see https://team-1617704806227.atlassian.net/browse/MIP-634",
+    ),
+)
+def test_logisticregression_cv_algorithm(test_input, expected, subtests):
+    response = algorithm_request("logistic_regression_cv", test_input)
+    try:
+        result = json.loads(response.text)
+    except json.decoder.JSONDecodeError:
+        raise ValueError(f"The result is not valid json:\n{response.text}") from None
+
+    # summary results also contain their average and stdev in the last
+    # positions, so I remove them
+    summary = result["summary"]
+    accuracy_res = summary["accuracy"][:-2]
+    accuracy_exp = expected["accuracy"]
+    recall_res = summary["recall"][:-2]
+    recall_exp = expected["recall"]
+    precision_res = summary["precision"][:-2]
+    precision_exp = expected["precision"]
+    fscore_res = summary["fscore"][:-2]
+    fscore_exp = expected["fscore"]
+    auc_res = [rc["auc"] for rc in result["roc_curves"]]
+    auc_exp = expected["auc"]
+
+    with subtests.test():
+        # ttest results in NaNs when everything is 0, see answer below
+        # https://stats.stackexchange.com/questions/111423/what-is-the-p-value-for-paired-t-test-if-the-two-set-of-data-are-identical
+        # This happens when our model and sklearn's both predict zero
+        # positives or zero negatives. This is a degenerate case, but happens
+        # in random tests where the choice of variables doesn't always make
+        # sense.
+        if all(r == e for r, e in zip(accuracy_res, accuracy_exp)):
+            pass
+        else:
+            ttest = st.ttest_ind(a=accuracy_res, b=accuracy_exp)
+            assert ttest.pvalue >= 0.05
+    with subtests.test():
+        if all(r == e for r, e in zip(recall_res, recall_exp)):
+            pass
+        else:
+            ttest = st.ttest_ind(a=recall_res, b=recall_exp)
+            assert ttest.pvalue >= 0.05
+    with subtests.test():
+        if all(r == e for r, e in zip(precision_res, precision_exp)):
+            pass
+        else:
+            ttest = st.ttest_ind(a=precision_res, b=precision_exp)
+            assert ttest.pvalue >= 0.05
+    with subtests.test():
+        if all(r == e for r, e in zip(fscore_res, fscore_exp)):
+            pass
+        else:
+            ttest = st.ttest_ind(a=fscore_res, b=fscore_exp)
+            assert ttest.pvalue >= 0.05
+    with subtests.test():
+        if all(r == e for r, e in zip(auc_res, auc_exp)):
+            pass
+        else:
+            ttest = st.ttest_ind(a=auc_res, b=auc_exp)
+            assert ttest.pvalue >= 0.05
---
+++ tests/standalone_tests/test_metrics.py
@@ -0,0 +1,46 @@
+import numpy as np
+import pandas as pd
+import pytest
+import sklearn
+
+from mipengine.algorithms.metrics import _confusion_matrix_local
+from mipengine.algorithms.metrics import _get_tpr_fpr_from_counts
+from mipengine.algorithms.metrics import _roc_curve_local
+
+
+def test_confusion_matrix_local():
+    ytrue = pd.DataFrame({"ybin": [0, 0, 1, 1]})
+    proba = pd.DataFrame({"proba": [0.3, 0.7, 0.3, 0.7]})
+
+    result = _confusion_matrix_local(ytrue, proba)
+    expected = {
+        "confmat": {"data": [[1, 1], [1, 1]], "operation": "sum", "type": "int"}
+    }
+    assert result == expected
+
+
+@pytest.mark.slow
+def test_roc_curve():
+    """Randomized test, validates our `roc_curve` against sklearn's using 100
+    random inputs"""
+    for _ in range(100):
+        ytrue = pd.DataFrame({"ybin": np.random.randint(0, 2, size=100)})
+        proba = pd.DataFrame({"proba": np.random.rand(100)})
+
+        # expected results from sklearn
+        tpr_exp, fpr_exp, thresholds = sklearn.metrics.roc_curve(ytrue, proba)
+
+        # NOTE The reason for subtracting 1e-8 from thresholds is an aparent
+        # inconsistency in sklearn's implementation. The function `binarize` uses a
+        # condition with stricly greater than, see
+        # https://github.com/scikit-learn/scikit-learn/blob/36958fb240fbe435673a9e3c52e769f01f36bec0/sklearn/preprocessing/_data.py#L1996
+        # whereas `roc_curve` uses equal or greater than, see
+        # https://github.com/scikit-learn/scikit-learn/blob/36958fb240fbe435673a9e3c52e769f01f36bec0/sklearn/metrics/_ranking.py#L724
+        thresholds = thresholds - 1e-8
+
+        # computed results
+        result = _roc_curve_local(ytrue, proba, thresholds)
+        counts = {key: val["data"] for key, val in result.items()}
+        tpr_res, fpr_res = _get_tpr_fpr_from_counts(counts)
+        assert tpr_res == tpr_res
+        assert fpr_res == fpr_res
