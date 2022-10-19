c326afae259a6478e08d99d3587f3c2ff287172b
kfilippopolitis <kostasfilippop@gmail.com>
2022-10-19 17:18:44 +0300

    descriptive_statistics to descriptive_stats

--- tests/algorithm_validation_tests/test_descriptive_stats.py
+++ tests/algorithm_validation_tests/test_descriptive_stats.py
@@ -7,14 +7,12 @@ from tests.algorithm_validation_tests.helpers import algorithm_request
 from tests.algorithm_validation_tests.helpers import assert_allclose
 from tests.algorithm_validation_tests.helpers import get_test_params
 
-expected_file = (
-    Path(__file__).parent / "expected" / "descriptive_statistics_expected.json"
-)
+expected_file = Path(__file__).parent / "expected" / "descriptive_stats_expected.json"
 
 
 @pytest.mark.parametrize("test_input, expected", get_test_params(expected_file))
 def test_descriptive_stats(test_input, expected):
-    response = algorithm_request("descriptive_statistics", test_input)
+    response = algorithm_request("descriptive_stats", test_input)
     try:
         result = json.loads(response.text)
     except json.decoder.JSONDecodeError:
