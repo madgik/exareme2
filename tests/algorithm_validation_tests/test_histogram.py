dae7119658e9b9f9542170f6b4bbc26c3927045a
Apostolos Glenis <apostglen46@gmail.com>
2022-11-09 13:29:25 +0200

    added tests

---
+++ tests/algorithm_validation_tests/test_histogram.py
@@ -0,0 +1,29 @@
+import json
+from pathlib import Path
+
+import pytest
+
+from tests.algorithm_validation_tests.helpers import algorithm_request
+from tests.algorithm_validation_tests.helpers import assert_allclose
+from tests.algorithm_validation_tests.helpers import get_test_params
+
+expected_file = Path(__file__).parent / "expected" / "expected_histogram.json"
+
+
+@pytest.mark.parametrize("test_input, expected", get_test_params(expected_file))
+def test_histogram(test_input, expected):
+    response = algorithm_request("histogram", test_input)
+    try:
+        result = json.loads(response.text)
+    except json.decoder.JSONDecodeError:
+        raise ValueError(f"The result is not valid json:\n{response.text}") from None
+
+    # sort records by variable and dataset in order to compare them
+    expected_numerical = expected["numerical"]
+    expected_categorical = expected["categorical"]
+
+    result_numerical = result["numerical"]
+    result_categorical = result["categorical"]
+
+    assert expected_numerical == result_numerical
+    assert expected_categorical == result_categorical
