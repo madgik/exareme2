552e2b444a9d3cb07c8e4d70246af3f79202367a
simos <simospm@di.uoa.gr>
2022-11-10 17:01:10 +0200

    fixes on tests

--- tests/algorithm_validation_tests/test_logisticregression_cv_validation.py
+++ tests/algorithm_validation_tests/test_logisticregression_cv_validation.py
@@ -16,11 +16,14 @@ expected_file = Path(__file__).parent / "expected" / f"{algorithm_name}_expected
     "test_input, expected",
     get_test_params(
         expected_file,
-        skip_indices=[12, 17, 18, 19],  # [5, 6, 9, 12, 18, 19, 22, 17]
+        skip_indices=[3, 5, 12, 17, 18, 19],
         skip_reason="Tests 5, 9, 12, 18, 19, 22 results in empty tables, "
         "https://team-1617704806227.atlassian.net/browse/MIP-634.\n"
-        "Test 3, 17 fails to converge in CI, "
-        "https://team-1617704806227.atlassian.net/browse/MIP-680.",
+        "Test 17 fails to converge in CI, but succeeds when run locally"
+        "https://team-1617704806227.atlassian.net/browse/MIP-680."
+        "Test 5 seems to fail only on the one-node deployment(local & CI deployment)"
+        "Test 3 seems to fail only on CI (but succeeds when run locally) and only for "
+        "one-node deployment",
     ),
 )
 def test_logisticregression_cv_algorithm(test_input, expected, subtests):
