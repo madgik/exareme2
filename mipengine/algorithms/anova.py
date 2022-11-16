de7f983a9f1eb664bea975d8068c9fe7e20af4d0
molly <katsouli.mo@gmail.com>
2022-11-16 12:55:58 +0200

    New formulas in place

--- mipengine/algorithms/anova.py
+++ mipengine/algorithms/anova.py
@@ -28,16 +28,21 @@ def run(algo_interface):
     global_run = algo_interface.run_udf_on_global_node
 
     xvars, yvars = algo_interface.x_variables, algo_interface.y_variables
-    X, y = algo_interface.create_primary_data_views([xvars, yvars])
+    [data] = algo_interface.create_primary_data_views([yvars + xvars])
     covar_enums_x1 = list(algo_interface.metadata[xvars[0]]["enumerations"])
     covar_enums_x2 = list(algo_interface.metadata[xvars[1]]["enumerations"])
     sstype = algo_interface.algorithm_parameters["sstype"]
 
+    if len(covar_enums_x1) < 2:
+        raise ValueError("Cannot perform Anova when there is only one level")
+
+    if len(covar_enums_x2) < 2:
+        raise ValueError("Cannot perform Anova when there is only one level")
+
     sec_local_transfer, local_transfers = local_run(
         func=local1,
         keyword_args=dict(
-            y=y,
-            x=X,
+            data=data,
             covar_enums_x1=covar_enums_x1,
             covar_enums_x2=covar_enums_x2,
             sstype=sstype,
@@ -68,189 +73,56 @@ T = TypeVar("T")
 
 
 @udf(
-    y=relation(schema=S),
-    x=relation(schema=T),
+    data=relation(schema=T),
     covar_enums_x1=literal(),
     covar_enums_x2=literal(),
     sstype=literal(),
     return_type=[secure_transfer(sum_op=True, min_op=True, max_op=True), transfer()],
 )
-def local1(y, x, covar_enums_x1, covar_enums_x2, sstype):
+def local1(data, covar_enums_x1, covar_enums_x2, sstype):
     import numpy as np
     import pandas as pd
 
-    n_obs = y.shape[0]
-    variable = y.reset_index(drop=True).to_numpy().squeeze()
-    var_label = y.columns.values.tolist()[0]
-    covar_label_x1 = x.columns[0]
-    covar_label_x2 = x.columns[1]
-    covar_x1 = x[covar_label_x1].values.tolist()
-    covar_x2 = x[covar_label_x2].values.tolist()
+    n_obs = data.shape[0]
+    var_label = data.columns[0]
+    covar_label_x1 = data.columns[1]
+    covar_label_x2 = data.columns[2]
     covar_enums_x1 = np.array(covar_enums_x1)
     covar_enums_x2 = np.array(covar_enums_x2)
 
-    # Create dataframe
-    dataset = pd.DataFrame()
-    dataset[var_label] = variable
-    dataset[covar_label_x1] = covar_x1
-    dataset[covar_label_x2] = covar_x2
-    var_sq = var_label + "_sq"
-    dataset[var_sq] = variable**2
-
-    # Calculate degrees of freedom
-    dof_x1 = len(covar_enums_x1) - 1
-    dof_x2 = len(covar_enums_x2) - 1
-    dof_x1x2 = dof_x1 * dof_x2
-    dof_w = n_obs - (len(covar_enums_x1) * len(covar_enums_x2))
-
-    sum_y = sum(y[var_label])
-    sum_y_sqrd = sum(y[var_label] ** 2)
-
-    # get overall stats
-    overall_stats = dataset[var_label].agg(["count", "sum"])
-    overall_ssq = dataset[var_sq].sum()
-    overall_stats = overall_stats.append(pd.Series(data=overall_ssq, index=["sum_sq"]))
-
-    group_stats_x1 = (
-        dataset[[var_label, covar_label_x1]]
-        .groupby(covar_label_x1)
-        .agg(["count", "sum"])
-    )
-    group_stats_x1.columns = ["count", "sum"]
-    group_ssq_x1 = dataset[[var_sq, covar_label_x1]].groupby(covar_label_x1).sum()
-    group_ssq_x1.columns = ["sum_sq"]
-    group_stats_x1["group_ssq_x1"] = group_ssq_x1
-
-    group_stats_x2 = (
-        dataset[[var_label, covar_label_x2]]
-        .groupby(covar_label_x2)
-        .agg(["count", "sum"])
-    )
-    group_stats_x2.columns = ["count", "sum"]
-    group_ssq_x2 = dataset[[var_sq, covar_label_x2]].groupby(covar_label_x2).sum()
-    group_ssq_x2.columns = ["sum_sq"]
-    group_stats_x2["group_ssq_x2"] = group_ssq_x2
-
-    group_stats_df_y_x1 = pd.DataFrame(group_stats_x1)
-    group_stats_df_y_x2 = pd.DataFrame(group_stats_x2)
-    group_by_x1_x2 = dataset.groupby([covar_label_x1, covar_label_x2]).agg(
+    data["var_sq"] = data[var_label] ** 2
+    group_by_x1_x2 = data.groupby([covar_label_x1, covar_label_x2]).agg(
         ["count", "sum"]
     )
 
-    # Find differences between enumerations and current index of data to find discrepancies
-    diff_x1 = list(set(covar_enums_x1) - set(group_stats_x1.index))
-    diff_x2 = list(set(covar_enums_x2) - set(group_stats_x2.index))
-
-    if diff_x1:
-        diff_df_y_x1 = pd.DataFrame(
-            0,
-            index=diff_x1,
-            columns=["count", "sum", "group_ssq"],
-        )
-        group_stats_df_y_x1 = group_stats_x1.append(diff_df_y_x1)
-    if diff_x2:
-        diff_df_y_x2 = pd.DataFrame(
-            0,
-            index=diff_x2,
-            columns=["count", "sum", "group_ssq"],
-        )
-        group_stats_df_y_x2 = diff_df_y_x2.append(group_stats_x2)
-
-    group_stats_df_y_x1["groups"] = pd.Categorical(
-        group_stats_df_y_x1.index, categories=covar_enums_x1.tolist(), ordered=True
-    )
-    group_stats_df_y_x1.sort_values("groups", inplace=True)
-
-    group_stats_df_y_x2["groups"] = pd.Categorical(
-        group_stats_df_y_x2.index, categories=covar_enums_x2.tolist(), ordered=True
-    )
-    group_stats_df_y_x2.sort_values("groups", inplace=True)
-
     sec_transfer_ = {}
-    sec_transfer_["n_obs"] = {"data": n_obs, "operation": "sum", "type": "int"}
-    sec_transfer_["sum_y"] = {
-        "data": sum_y,
-        "operation": "sum",
-        "type": "float",
-    }
-    sec_transfer_["sum_y_sqrd"] = {
-        "data": sum_y_sqrd,
-        "operation": "sum",
-        "type": "float",
-    }
-    sec_transfer_["overall_stats_sum"] = {
-        "data": overall_stats["sum"].tolist(),
-        "operation": "sum",
-        "type": "float",
-    }
-    sec_transfer_["overall_stats_count"] = {
-        "data": overall_stats["count"].tolist(),
-        "operation": "sum",
-        "type": "float",
-    }
-    sec_transfer_["overall_ssq"] = {
-        "data": overall_ssq.item(),
-        "operation": "sum",
-        "type": "float",
-    }
-    sec_transfer_["group_stats_x1_sum"] = {
-        "data": group_stats_df_y_x1["sum"].tolist(),
-        "operation": "sum",
-        "type": "float",
-    }
-    sec_transfer_["group_stats_x1_count"] = {
-        "data": group_stats_df_y_x1["count"].tolist(),
-        "operation": "sum",
-        "type": "float",
-    }
-    sec_transfer_["group_stats_x1_ssq"] = {
-        "data": group_stats_df_y_x1["group_ssq_x1"].tolist(),
-        "operation": "sum",
-        "type": "float",
-    }
-    sec_transfer_["group_stats_x2_sum"] = {
-        "data": group_stats_df_y_x2["sum"].tolist(),
-        "operation": "sum",
-        "type": "float",
-    }
-    sec_transfer_["group_stats_x2_count"] = {
-        "data": group_stats_df_y_x2["count"].tolist(),
-        "operation": "sum",
-        "type": "float",
-    }
-    sec_transfer_["group_stats_x2_ssq"] = {
-        "data": group_stats_df_y_x2["group_ssq_x2"].tolist(),
-        "operation": "sum",
-        "type": "float",
-    }
-    sec_transfer_["group_by_x1_x2_sum"] = {
-        "data": group_by_x1_x2[var_label]["sum"].tolist(),
-        "operation": "sum",
-        "type": "float",
-    }
-    sec_transfer_["group_by_x1_x2_count"] = {
+    sec_transfer_["y_per_x1_x2_group_count"] = {
         "data": group_by_x1_x2[var_label]["count"].tolist(),
         "operation": "sum",
         "type": "float",
     }
-    sec_transfer_["group_by_x1_x2_index"] = {
-        "data": group_by_x1_x2[var_label].index.tolist(),
+    sec_transfer_["y_per_x1_x2_group_sum"] = {
+        "data": group_by_x1_x2[var_label]["sum"].tolist(),
+        "operation": "sum",
+        "type": "float",
+    }
+    sec_transfer_["y_per_x1_x2_group_ssq"] = {
+        "data": group_by_x1_x2["var_sq"]["sum"].tolist(),
+        "operation": "sum",
+        "type": "float",
+    }
+    sec_transfer_["table_index"] = {
+        "data": group_by_x1_x2.index.tolist(),
         "operation": "sum",
         "type": "float",
     }
 
     transfer_ = {
         "var_label": var_label,
-        "dof_w": dof_w,
-        "dof_x1": dof_x1,
-        "dof_x2": dof_x2,
-        "dof_x1x2": dof_x1x2,
         "covar_label_x1": covar_label_x1,
         "covar_label_x2": covar_label_x2,
         "covar_enums_x1": covar_enums_x1.tolist(),
         "covar_enums_x2": covar_enums_x2.tolist(),
-        "group_stats_df_y_x1_index": group_stats_df_y_x1.index.tolist(),
-        "group_stats_df_y_x2_index": group_stats_df_y_x2.index.tolist(),
     }
 
     return sec_transfer_, transfer_
@@ -262,85 +134,60 @@ def local1(y, x, covar_enums_x1, covar_enums_x2, sstype):
     return_type=[transfer()],
 )
 def global1(sec_local_transfer, local_transfers):
-    import itertools
-
     import numpy as np
+    import pandas as pd
 
-    n_obs = sec_local_transfer["n_obs"]
-    sum_y = sec_local_transfer["sum_y"]
-    group_stats_x1_sum = np.array(sec_local_transfer["group_stats_x1_sum"])
-    group_stats_x1_count = np.array(sec_local_transfer["group_stats_x1_count"])
-    group_ssq_x1 = np.array(sec_local_transfer["group_stats_x1_ssq"])
-    group_stats_x2_sum = np.array(sec_local_transfer["group_stats_x2_sum"])
-    group_stats_x2_count = np.array(sec_local_transfer["group_stats_x2_count"])
-    group_ssq_x2 = np.array(sec_local_transfer["group_stats_x2_ssq"])
+    group_stats_sum = np.array(sec_local_transfer["y_per_x1_x2_group_sum"])
+    group_stats_count = np.array(sec_local_transfer["y_per_x1_x2_group_count"])
+    group_stats_ssq = np.array(sec_local_transfer["y_per_x1_x2_group_ssq"])
+    group_table_index = np.array(sec_local_transfer["table_index"])
+    covar_enums_x1 = local_transfers[0]["covar_enums_x1"]
+    covar_enums_x2 = local_transfers[0]["covar_enums_x2"]
+    var_label = [t["var_label"] for t in local_transfers][0]
+    covar_label_x1 = [t["covar_label_x1"] for t in local_transfers][0]
+    covar_label_x2 = [t["covar_label_x2"] for t in local_transfers][0]
 
-    group_stats_df_y_x1_index = [
-        t["group_stats_df_y_x1_index"] for t in local_transfers
-    ]
-    group_stats_df_y_x2_index = [
-        t["group_stats_df_y_x2_index"] for t in local_transfers
-    ]
+    res_dict = {
+        "x1_index": group_table_index.T[0],
+        "x2_index": group_table_index.T[1],
+        "n": group_stats_count,
+        "sx": group_stats_sum,
+        "sxx": group_stats_ssq,
+    }
 
-    group_stats_index_x1 = group_stats_df_y_x1_index[0]
-    if len(group_stats_df_y_x1_index) > 1:
-        for x, y in itertools.combinations(group_stats_df_y_x1_index, 2):
-            group_stats_index_x1 = x
-            diff = list(set(x) - set(y))
-            if diff != []:
-                group_stats_index_x1.append(diff)
+    df = pd.DataFrame(res_dict)
+    df.set_index(["x1_index"])
 
-    group_stats_index_x2 = group_stats_df_y_x2_index[0]
-    if len(group_stats_df_y_x2_index) > 1:
-        for x, y in itertools.combinations(group_stats_df_y_x2_index, 2):
-            group_stats_index_x2 = x
-            diff = list(set(x) - set(y))
-            if diff != []:
-                group_stats_index_x2.append(diff)
+    n = df.pivot(index="x1_index", columns="x2_index", values="n")
+    sx = df.pivot(index="x1_index", columns="x2_index", values="sx")
+    sxx = df.pivot(index="x1_index", columns="x2_index", values="sxx")
 
-    # remove zero count groups
-    df_y_x1 = pd.DataFrame(
-        {
-            "groups": group_stats_index_x1,
-            "count": group_stats_x1_count,
-            "ssq": group_ssq_x1,
-            "sum": group_stats_x1_sum,
-        },
-        index=group_stats_index_x1,
-    )
-    if not np.all(group_stats_x1_count):
-        df_y_x1 = df_y_x1[df_y_x1["count"] != 0]
-        group_stats_x1_count = np.array(df_y_x1["count"])
-        group_stats_x1_sum = np.array(df_y_x1["sum"])
+    nh_denom = sum(1 / df["n"])
+    n_harmonic = (len(covar_enums_x1) * len(covar_enums_x2)) / nh_denom
+    ss_cell = sxx - (sx**2) / n
 
-    # remove zero count groups
-    df_y_x2 = pd.DataFrame(
-        {
-            "groups": group_stats_index_x2,
-            "count": group_stats_x2_count,
-            "ssq": group_ssq_x2,
-            "sum": group_stats_x2_sum,
-        },
-        index=group_stats_index_x2,
-    )
-    if not np.all(group_stats_x2_count):
-        df_y_x2 = df_y_x2[df_y_x2["count"] != 0]
-        group_stats_x2_count = np.array(df_y_x2["count"])
-        group_stats_x2_sum = np.array(df_y_x2["sum"])
+    cell_means = sx / n
+    total_means_x1 = cell_means.sum(axis=1)
+    total_means_x2 = cell_means.sum(axis=0)
+    all_means = total_means_x1.tolist() + total_means_x2.tolist()
+    all_means_sq = [j**2 for j in all_means]
+    s_x1_sq = sum(total_means_x1**2) / len(covar_enums_x2)
+    s_x2_sq = sum(total_means_x2**2) / len(covar_enums_x1)
+    s_x1x2_sq = sum(all_means_sq)
+    n_obs = sum(df["n"])
+    sum_y = sum(df["sx"])
+    sum_y_ssq = sum(df["sxx"])
+    grand_mean = sum(total_means_x1) ** 2 / (len(covar_enums_x1) * len(covar_enums_x2))
+    ssq_x1 = n_harmonic * (s_x1_sq - grand_mean)
+    ssq_x2 = n_harmonic * (s_x2_sq - grand_mean)
+    ssq_x1x2 = n_harmonic * (s_x1x2_sq - s_x1_sq - s_x2_sq + grand_mean)
+    # raise ValueError(ssq_x1, ssq_x2, s_x1x2_sq)
 
-    categories_x1 = local_transfers[0]["covar_enums_x1"]
-    if len(categories_x1) < 2:
-        raise ValueError("Cannot perform Anova when there is only one level")
-
-    categories_x2 = local_transfers[0]["covar_enums_x2"]
-    if len(categories_x2) < 2:
-        raise ValueError("Cannot perform Anova when there is only one level")
-
-    grand_mean = sum_y / n_obs
-    df_y_x1["mean"] = group_stats_x1_sum / group_stats_x1_count
-    df_y_x2["mean"] = group_stats_x2_sum / group_stats_x2_count
-    ssq_x1 = sum((df_y_x1["mean"] - grand_mean) ** 2)
-    ssq_x2 = sum((df_y_x2["mean"] - grand_mean) ** 2)
+    # Calculate degrees of freedom
+    dof_x1 = len(covar_enums_x1) - 1
+    dof_x2 = len(covar_enums_x2) - 1
+    dof_x1x2 = dof_x1 * dof_x2
+    dof_w = n_obs - (len(covar_enums_x1) * len(covar_enums_x2))
 
     transfer_ = {
         "n_obs": n_obs,
