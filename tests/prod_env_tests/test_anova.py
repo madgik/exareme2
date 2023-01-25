6356cf06f09936c25852c5aa9d0ac1f93f531fa0
Jason Sakellariou <js.sakel@gmail.com>
2023-01-25 10:59:18 +0200

    Two-way Anova: validation
    https://team-1617704806227.atlassian.net/browse/MIP-675

---
+++ tests/prod_env_tests/test_anova.py
@@ -0,0 +1,18 @@
+import json
+from pathlib import Path
+
+import pytest
+
+from tests.algorithm_validation_tests.helpers import algorithm_request
+from tests.algorithm_validation_tests.helpers import get_test_params
+from tests.algorithm_validation_tests.test_anova import validate_results
+
+expected_file = Path(__file__).parent / "expected" / "anova_twoway_prod.json"
+
+
+@pytest.mark.parametrize("test_input, expected", get_test_params(expected_file))
+def test_anova_two_way(test_input, expected, subtests):
+    response = algorithm_request("anova", test_input)
+    result = json.loads(response.content)
+
+    validate_results(result, expected, subtests)
