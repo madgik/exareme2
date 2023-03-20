f632efc6d9b616e67183f7e5e022e1578fbdcbe1
kfilippopolitis <kostasfilippop@gmail.com>
2023-03-20 15:09:55 +0200

    Now we configure users only at the initialization.

--- monetdb/bootstrap.sh
+++ monetdb/bootstrap.sh
@@ -22,6 +22,8 @@ then
   monetdb set mal_for_all=yes db
   monetdb release db
   monetdb start db
+  ./configure_users.sh
+
   echo 'Database initialized.'
 else
   echo 'Checking if previous instances are still running (from other containers).'
@@ -42,7 +44,6 @@ else
   echo 'Database restarted.'
 fi
 
-./configure_users.sh
 ./configure_monit.sh
 
 echo 'MonetDB merovingian logs:'
