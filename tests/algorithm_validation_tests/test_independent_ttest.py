5c86ee5652706412b30c66dc8048874a4cced2cf
molly <katsouli.mo@gmail.com>
2023-04-28 22:29:44 +0300

    Skips failing tests in independent t-test and adds reason

--- tests/algorithm_validation_tests/test_independent_ttest.py
+++ tests/algorithm_validation_tests/test_independent_ttest.py
@@ -12,7 +12,14 @@ algorithm_name = "ttest_independent"
 expected_file = Path(__file__).parent / "expected" / f"{algorithm_name}_expected.json"
 
 
-@pytest.mark.parametrize("test_input, expected", get_test_params(expected_file))
+@pytest.mark.parametrize(
+    "test_input, expected",
+    get_test_params(
+        expected_file,
+        skip_indices=[4],
+        skip_reason="P-value assertion fail. Related ticket: https://team-1617704806227.atlassian.net/browse/MIP-794",
+    ),
+)
 def test_independent_ttest(test_input, expected, subtests):
     response = algorithm_request(algorithm_name, test_input)
     result = parse_response(response)
