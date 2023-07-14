9a27b11b4f053c0f623e408254df2530352b20cf
ThanKarab <tkarabatsis@hotmail.com>
2023-07-14 19:10:31 +0300

    Moved longitudinal transformer tests to prod_env tests.
    These tests were not testing the validity of the algorithms just their execution.
    The longitudinal_dementia data were also broken into 3 csvs so that we will actually test the federation.

--- tests/prod_env_tests/test_get_data_models_attributes.py
+++ tests/prod_env_tests/test_get_data_models_attributes.py
@@ -10,7 +10,7 @@ def test_get_data_models_attributes():
     response = json.loads(request.text)
 
     for data_model, attributes in response.items():
-        assert data_model in ["dementia:0.1", "tbi:0.1"]
+        assert data_model in ["dementia:0.1", "tbi:0.1", "longitudinal_dementia:0.1"]
         assert all([elem in attributes for elem in ["tags", "properties"]])
         assert "cdes" in attributes["properties"]
         assert len(attributes["properties"]["cdes"]) > 0
