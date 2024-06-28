5db2f7d98af0e6862551eb96e3a46c6874dcd098
molly <katsouli.mo@gmail.com>
2024-06-28 17:20:13 +0300

    Fixes comments from pr

--- exareme2/algorithms/exareme2/pca_with_tranformations.py
+++ exareme2/algorithms/exareme2/pca_with_tranformations.py
@@ -1,13 +1,17 @@
 from typing import List
 from typing import TypeVar
 
-# import numpy
 from pydantic import BaseModel
 
 from exareme2 import DType
 from exareme2.algorithms.exareme2.algorithm import Algorithm
 from exareme2.algorithms.exareme2.algorithm import AlgorithmDataLoader
 from exareme2.algorithms.exareme2.helpers import get_transfer_data
+from exareme2.algorithms.exareme2.pca import PCAResult
+from exareme2.algorithms.exareme2.pca import global1
+from exareme2.algorithms.exareme2.pca import global2
+from exareme2.algorithms.exareme2.pca import local1
+from exareme2.algorithms.exareme2.pca import local2
 from exareme2.algorithms.exareme2.udfgen import DEFERRED
 from exareme2.algorithms.exareme2.udfgen import literal
 from exareme2.algorithms.exareme2.udfgen import merge_transfer
@@ -26,18 +30,8 @@ class PCADataLoader(AlgorithmDataLoader, algname=ALGORITHM_NAME):
         return [self._variables.y]
 
 
-class PCAResult(BaseModel):
-    title: str
-    n_obs: int
-    eigenvalues: List[float]
-    eigenvectors: List[List[float]]
-
-
 class PCAAlgorithm(Algorithm, algname=ALGORITHM_NAME):
     def run(self, data, metadata):
-        local_run = self.engine.run_udf_on_local_workers
-        global_run = self.engine.run_udf_on_global_worker
-
         [X_relation] = data
 
         if "data_transformation" in self.algorithm_parameters:
@@ -63,20 +57,19 @@ class PCAAlgorithm(Algorithm, algname=ALGORITHM_NAME):
         output_schema += [(colname, DType.FLOAT) for colname in X_relation.columns]
 
         try:
-            if any(trans in data_transformation for trans in ["log", "exp"]):
-                X_relation = local_run(
-                    func=local_data_processing,
-                    keyword_args={
-                        "data": X_relation,
-                        "data_transformation_dict": {
-                            k: v
-                            for k, v in data_transformation.items()
-                            if k in ["log", "exp"]
-                        },
+            X_relation = local_run(
+                func=local_data_processing,
+                keyword_args={
+                    "data": X_relation,
+                    "data_transformation_dict": {
+                        k: v
+                        for k, v in data_transformation.items()
+                        if k in ["log", "exp"]
                     },
-                    output_schema=output_schema,
-                    share_to_global=[False],
-                )
+                },
+                output_schema=output_schema,
+                share_to_global=[False],
+            )
         except Exception as ex:
             self.handle_data_transformation_exceptions(ex)
 
@@ -98,34 +91,33 @@ class PCAAlgorithm(Algorithm, algname=ALGORITHM_NAME):
         data_transformation = self.algorithm_parameters.get("data_transformation", {})
 
         try:
-            if any(trans in data_transformation for trans in ["standardize", "center"]):
-                local_transfers_stats = local_run(
-                    func=local_stats,
-                    keyword_args={
-                        "x": X_relation,
+            local_transfers_stats = local_run(
+                func=local_stats,
+                keyword_args={
+                    "x": X_relation,
+                },
+                share_to_global=[True],
+            )
+            _, global_stats_transfer = global_run(
+                func=global_stats,
+                keyword_args={"local_transfers": local_transfers_stats},
+                share_to_locals=[True, True],
+            )
+            X_relation = local_run(
+                func=local_transform,
+                keyword_args={
+                    "x": X_relation,
+                    "global_transfer": global_stats_transfer,
+                    "data_transformation_dict": {
+                        k: v
+                        for k, v in data_transformation.items()
+                        if k in ["center", "standardize"]
                     },
-                    share_to_global=[True],
-                )
-                self.global_stats_state, global_stats_transfer = global_run(
-                    func=global_stats,
-                    keyword_args={"local_transfers": local_transfers_stats},
-                    share_to_locals=[True, True],
-                )
-                X_relation = local_run(
-                    func=local_transform,
-                    keyword_args={
-                        "x": X_relation,
-                        "global_transfer": global_stats_transfer,
-                        "data_transformation_dict": {
-                            k: v
-                            for k, v in data_transformation.items()
-                            if k in ["center", "standardize"]
-                        },
-                    },
-                    output_schema=[("row_id", DType.INT)]
-                    + [(colname, DType.FLOAT) for colname in X_relation.columns],
-                    share_to_global=[False],
-                )
+                },
+                output_schema=[("row_id", DType.INT)]
+                + [(colname, DType.FLOAT) for colname in X_relation.columns],
+                share_to_global=[False],
+            )
         except Exception as ex:
             self.handle_standardize_and_center_exceptions(ex)
 
@@ -175,88 +167,6 @@ class PCAAlgorithm(Algorithm, algname=ALGORITHM_NAME):
 
 
 S = TypeVar("S")
-
-
-@udf(x=relation(schema=S), return_type=[secure_transfer(sum_op=True)])
-def local1(x):
-    n_obs = len(x)
-    sx = numpy.einsum("ij->j", x)
-    sxx = numpy.einsum("ij,ij->j", x, x)
-
-    transfer_ = {}
-    transfer_["n_obs"] = {"data": n_obs, "operation": "sum", "type": "int"}
-    transfer_["sx"] = {"data": sx.tolist(), "operation": "sum", "type": "float"}
-    transfer_["sxx"] = {"data": sxx.tolist(), "operation": "sum", "type": "float"}
-    return transfer_
-
-
-@udf(local_transfers=secure_transfer(sum_op=True), return_type=[state(), transfer()])
-def global1(local_transfers):
-    import numpy as np
-
-    n_obs = local_transfers["n_obs"]
-    sx = numpy.array(local_transfers["sx"])
-    sxx = numpy.array(local_transfers["sxx"])
-
-    means = sx / n_obs
-    sigmas = ((sxx - n_obs * means**2) / (n_obs - 1)) ** 0.5
-
-    state_ = dict(n_obs=n_obs)
-    transfer_ = dict(means=means.tolist(), sigmas=sigmas.tolist())
-    return state_, transfer_
-
-
-@udf(
-    x=relation(schema=S),
-    global_transfer=transfer(),
-    return_type=[secure_transfer(sum_op=True)],
-)
-def local2(x, global_transfer):
-    means = numpy.array(global_transfer["means"])
-    sigmas = numpy.array(global_transfer["sigmas"])
-
-    x = x.values
-    out = numpy.empty(x.shape)
-
-    numpy.subtract(x, means, out=out)
-    numpy.divide(out, sigmas, out=out)
-    gramian = numpy.einsum("ji,jk->ik", out, out)
-
-    transfer_ = {
-        "gramian": {
-            "data": gramian.tolist(),
-            "operation": "sum",
-            "type": "float",
-        }
-    }
-    return transfer_
-
-
-@udf(
-    local_transfers=secure_transfer(sum_op=True),
-    prev_state=state(),
-    return_type=[transfer()],
-)
-def global2(local_transfers, prev_state):
-    gramian = numpy.array(local_transfers["gramian"])
-
-    # Ensure n_obs is greater than 1 to avoid division by zero
-    n_obs = prev_state["n_obs"]
-
-    covariance = gramian / (n_obs - 1)
-
-    eigenvalues, eigenvectors = numpy.linalg.eig(covariance)
-    idx = eigenvalues.argsort()[::-1]
-    eigenvalues = eigenvalues[idx]
-    eigenvectors = eigenvectors[:, idx]
-    eigenvectors = eigenvectors.T
-
-    transfer_ = dict(
-        n_obs=n_obs,
-        eigenvalues=eigenvalues.tolist(),
-        eigenvectors=eigenvectors.tolist(),
-    )
-    return transfer_
 
 
 @udf(
